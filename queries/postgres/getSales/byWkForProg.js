1d720cef9522296de6d7cf9235712ba2854c15fb
Eastern Dev <dev@seaflow.fish>
2023-03-08 07:54:16 -0500

    building out major and minor rows

--- queries/postgres/getSales/byWkForProg.js
+++ queries/postgres/getSales/byWkForProg.js
@@ -8,7 +8,7 @@ const getWklySalesByProg = async (program, fy) => {
     console.log(`query postgres to get weekly purchses for ${program} ...`)
 
     const response = await pgClient.query(
-      'SELECT sales_line_items.week_serial AS week_serial, SUM(sales_line_items.calc_gm_rept_weight) AS lbs, SUM(sales_line_items.calc_gl_gross_sales) AS sales, SUM(sales_line_items.calc_gl_cogs) AS cogs, SUM(sales_line_items.calc_gl_othp) AS othp, \'TOTAL\' AS maj_row FROM "salesReporting".sales_line_items WHERE sales_line_items.program = $1 AND sales_line_items.fiscal_year = $2 GROUP BY sales_line_items.week_serial, maj_row ORDER BY sales_line_items.week_serial',
+      'SELECT sales_line_items.week_serial AS week_serial, SUM(sales_line_items.calc_gm_rept_weight) AS lbs, SUM(sales_line_items.calc_gl_gross_sales) AS sales, SUM(sales_line_items.calc_gl_cogs) AS cogs, SUM(sales_line_items.calc_gl_othp) AS othp, \'total\' AS min_row, $1 AS maj_row FROM "salesReporting".sales_line_items WHERE sales_line_items.program = $1 AND sales_line_items.fiscal_year = $2 GROUP BY sales_line_items.week_serial, maj_row ORDER BY sales_line_items.week_serial',
       [program, fy]
     )
 
