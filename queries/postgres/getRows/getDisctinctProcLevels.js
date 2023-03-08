a921b8e3138c3ae6f44e756b80c324249b2c6474
Eastern Dev <dev@seaflow.fish>
2023-03-08 07:18:10 -0500

    building out major and minor rows

--- queries/postgres/getRows/getDisctinctProcLevels.js
+++ queries/postgres/getRows/getDisctinctProcLevels.js
@@ -7,7 +7,7 @@ const getDistinctProcLevels = async (program, fy) => {
     console.log(`query postgres master supplement to get proc levels for ${program} to build rows template ...`)
 
     const response = await pgClient.query(
-      'SELECT DISTINCT(TRIM(sales_line_items.fg_treatment)) AS \'min_row\' FROM "salesReporting".sales_line_items WHERE sales_line_items.item_type = $1 AND sales_line_items.program = $2 AND sales_line_items.byproduct_type IS NULL AND sales_line_items.fiscal_year = $3',
+      'SELECT DISTINCT(TRIM(sales_line_items.fg_treatment)) AS min_row, \'FG\' AS maj_row FROM "salesReporting".sales_line_items WHERE sales_line_items.item_type = $1 AND sales_line_items.program = $2 AND sales_line_items.byproduct_type IS NULL AND sales_line_items.fiscal_year = $3',
       ['FG', program, fy]
     )
 
