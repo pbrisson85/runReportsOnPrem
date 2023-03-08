dd0e3ccc1a922459f18ab967e273cd8946d977d0
Eastern Dev <dev@seaflow.fish>
2023-03-08 07:05:40 -0500

    building out major and minor rows

--- queries/postgres/getDisctinctItemTypes.js
+++ queries/postgres/getDisctinctItemTypes.js
@@ -7,7 +7,7 @@ const getDistinctItemTypes = async (program, fy) => {
     console.log(`query postgres sales reporting to get item types for ${program} to build rows template ...`)
 
     const response = await pgClient.query(
-      'SELECT DISTINCT(TRIM(salesReporting.item_type)) AS maj_row, \'subtotal\' AS min_row FROM "salesReporting".sales_line_items WHERE sales_line_items.program = $1 AND sales_line_items.byproduct_type IS NULL AND sales_line_items.fiscal_year = $2',
+      'SELECT DISTINCT(TRIM(sales_line_items.item_type)) AS maj_row, \'subtotal\' AS min_row FROM "salesReporting".sales_line_items WHERE sales_line_items.program = $1 AND sales_line_items.byproduct_type IS NULL AND sales_line_items.fiscal_year = $2',
       [program, fy]
     )
 
