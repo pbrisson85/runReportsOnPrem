0be164aa15fffa966fcfac9bbd567a68b3df9e6d
Eastern Dev <dev@seaflow.fish>
2023-05-16 12:19:49 -0400

    testing switching over getWklyProgSales to join with inven supplement

--- queries/postgres/getRows/getDisctinctItemTypes.js
+++ queries/postgres/getRows/getDisctinctItemTypes.js
@@ -7,7 +7,7 @@ const getDistinctItemTypes = async (program, fy) => {
     console.log(`query postgres sales reporting to get item types for ${program} to build rows template ...`)
 
     const response = await pgClient.query(
-      'SELECT DISTINCT(TRIM(sales_line_items.item_type)) AS maj_row, \'subtotal\' AS min_row FROM "salesReporting".sales_line_items WHERE sales_line_items.program = $1 AND sales_line_items.byproduct_type IS NULL AND sales_line_items.fiscal_year = $2',
+      'SELECT DISTINCT(TRIM(master_supplement.item_type)) AS maj_row, \'subtotal\' AS min_row FROM "salesReporting".sales_line_items LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = sales_line_items.item_number WHERE master_supplement.program = $1 AND master_supplement.byproduct_type IS NULL AND sales_line_items.fiscal_year = $2',
       [program, fy]
     )
 
