77ff1a077cde1f972cd71ce7faf663644f721126
Eastern Dev <dev@seaflow.fish>
2023-05-23 13:30:32 -0400

    debugging running 2020 sales

--- models/formatPostgresDateForSeasoftQuery.js
+++ models/formatPostgresDateForSeasoftQuery.js
@@ -1,26 +1,12 @@
 const formatPostgresDateForSeasoftQuery = date => {
-  console.log('date', date)
-
   date = date.split('/')
   // results in [month, day, year]
 
-  console.log('date', date)
-  console.log('date[0]', date[0])
-  console.log('date[0].length', date[0].length)
-  console.log('date[0].trim()', date[0].trim())
-  console.log('date[0].trim().length', date[0].trim().length)
-
-  const y = date[2]
-  const m = date[0].length === 1 ? '0' + date[0] : date[0]
-  const d = date[1].length === 1 ? '0' + date[1] : date[1]
-
-  console.log('y', y)
-  console.log('m', m)
-  console.log('d', d)
+  const y = date[2].trim()
+  const m = date[0].trim().length === 1 ? '0' + date[0].trim() : date[0].trim()
+  const d = date[1].trim().length === 1 ? '0' + date[1].trim() : date[1].trim()
 
   const formattedDate = `{d '${y}-${m}-${d}'}`
-
-  console.log('formattedDate', formattedDate)
 
   // conver to {d 'yyyy-mm-dd'}
 
