524a123de669b8ecb1d8e937d3c74a42dd088ead
Eastern Dev <dev@seaflow.fish>
2023-05-23 17:38:35 -0400

    debugging running 2018 sales

--- queries/postgres/getFirstDayOfFiscalYear.js
+++ queries/postgres/getFirstDayOfFiscalYear.js
@@ -3,12 +3,14 @@ const getFirstDayOfFiscalYear = async fiscalYear => {
   const pgClient = new Client() // config from ENV
   await pgClient.connect()
 
-  console.log('query postgres for first day of fiscal year ...')
+  console.log(`query postgres for first day of fiscal year ${fiscalYear} ...`)
 
   const periodsByDay = await pgClient.query(
     "SELECT MIN(period_by_day.date::DATE) AS date FROM \"accountingPeriods\".period_by_day WHERE period_by_day.fiscal_year = $1", [fiscalYear]) //prettier-ignore
 
   await pgClient.end()
+
+  console.log(`fiorst day: ${periodsByDay.rows} ...`)
 
   return periodsByDay.rows
 }
