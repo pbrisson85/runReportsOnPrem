e2dfdd547b32b0a25da196ff70f0441bbfe6dca4
Eastern Dev <dev@seaflow.fish>
2023-05-24 16:02:22 -0400

    builting and testing getSalesByProgram

--- routines/wklyReportByProgram.js
+++ routines/wklyReportByProgram.js
@@ -300,8 +300,9 @@ const getWeeklyProgramSales = async (start, end) => {
   Object.keys(mappedSales).forEach((key, idx) => {
     if (idx === 0) return
 
-    console.log(key, idx)
-    console.log(mappedSales[key], mappedSales[idx - 1])
+    console.log('key', key)
+    console.log('idx - 1', idx - 1)
+    console.log(mappedSales[key].maj_row, mappedSales[idx - 1])
 
     if (mappedSales[key].maj_row === mappedSales[idx - 1].maj_row) {
       mappedSales[key].maj_row = ''
