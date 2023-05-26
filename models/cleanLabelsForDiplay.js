0966e77ab9774b7eac21781405611c1301c027d4
Eastern Dev <dev@seaflow.fish>
2023-05-26 07:24:57 -0400

    working on inventory cols

--- models/cleanLabelsForDiplay.js
+++ models/cleanLabelsForDiplay.js
@@ -5,8 +5,6 @@ const cleanLabelsForDisplay = flattenedMappedData => {
     // first row has species
     if (idx === 0) return row
 
-    if (typeof row.min_row === 'undefined' || idx === 1) console.log(row)
-
     // subtotal hit flip flag true but return hidden species flag
     if (row.min_row.toUpperCase() === 'SUBTOTAL') {
       subTotalFound = true
