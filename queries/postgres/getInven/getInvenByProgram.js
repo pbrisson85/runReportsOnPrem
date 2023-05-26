1311aff5b689685d708494f5d88bd79bc4d78b1e
Eastern Dev <dev@seaflow.fish>
2023-05-26 08:11:24 -0400

    working on inventory cols

--- queries/postgres/getInven/getInvenByProgram.js
+++ queries/postgres/getInven/getInvenByProgram.js
@@ -146,6 +146,80 @@ const getFgAtLocationBySepcies = async () => {
   }
 }
 
+/* *********************************************** TOTAL INVEN *********************************************** */
+
+// FG on hand grouped by species (includes in transit)
+
+const getFgTotal = async () => {
+  try {
+    const { Client } = require('pg')
+    const pgClient = new Client() // config from ENV
+    await pgClient.connect()
+
+    console.log(`query postgres for FG on hand ...`)
+
+    const response = await pgClient.query(
+      'SELECT \'FG INVEN\' AS column, \'FG SALES\' AS maj_row, \'TOTAL\' AS min_row, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS sales, SUM(perpetual_inventory.cost_extended) AS cogs, SUM(perpetual_inventory.cost_extended) AS othp FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory)',
+      ['FG']
+    )
+
+    await pgClient.end()
+
+    return response.rows
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+// FG in transit grouped by species
+
+const getFgInTransitTotal = async () => {
+  try {
+    const { Client } = require('pg')
+    const pgClient = new Client() // config from ENV
+    await pgClient.connect()
+
+    console.log(`query postgres for FG on hand ...`)
+
+    const response = await pgClient.query(
+      'SELECT \'IN TRANSIT\' AS column, \'FG SALES\' AS maj_row, \'TOTAL\' AS min_row, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS sales, SUM(perpetual_inventory.cost_extended) AS cogs, SUM(perpetual_inventory.cost_extended) AS othp FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND perpetual_inventory.location_type = $2',
+      ['FG', 'IN TRANSIT']
+    )
+
+    await pgClient.end()
+
+    return response.rows
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+// FG at location grouped by species
+
+const getFgAtLocationTotal = async () => {
+  try {
+    const { Client } = require('pg')
+    const pgClient = new Client() // config from ENV
+    await pgClient.connect()
+
+    console.log(`query postgres for FG on hand ...`)
+
+    const response = await pgClient.query(
+      'SELECT \'ON HAND\' AS column, \'FG SALES\' AS maj_row, \'TOTAL\' AS min_row, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS sales, SUM(perpetual_inventory.cost_extended) AS cogs, SUM(perpetual_inventory.cost_extended) AS othp FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND perpetual_inventory.location_type <> $2',
+      ['FG', 'IN TRANSIT']
+    )
+
+    await pgClient.end()
+
+    return response.rows
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
 module.exports.getFgByProgram = getFgByProgram
 module.exports.getFgInTransitByProgram = getFgInTransitByProgram
 module.exports.getFgAtLocationByProgram = getFgAtLocationByProgram
