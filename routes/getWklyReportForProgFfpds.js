aa0a964401e1ff6b29c09a0318724f6d04561fed
Eastern Dev <dev@seaflow.fish>
2023-06-07 07:57:18 -0400

    starting a new report for fresh/frozen, processed/dry, size

--- routes/getWklyReportForProgFfpds.js
+++ routes/getWklyReportForProgFfpds.js
@@ -9,16 +9,16 @@ const { getStartOfWeek } = require('../queries/postgres/generateSalesData/getDat
 // Generate full weekly report of One program for FG Only (1st level drill down)
 router.post('/', async (req, res) => {
   console.log(
-    `\nget get weekly sales for ${req.body.options.program}, by fresh/frozen, processed/dry, size during ${req.body.start} through ${req.body.end} route HIT...`
+    `\nget get weekly sales for ${req.body.program}, by fresh/frozen, processed/dry, size during ${req.body.start} through ${req.body.end} route HIT...`
   )
 
   // Note that start date is the END of the first week. Need the beginning of the same week to pull invoice dates that are after this:
   const startWeek = await getStartOfWeek(req.body.start)
 
-  const resp = await getWeeklyProgSalesFfpds(startWeek[0].formatted_date_start, req.body.end, req.body.options.program)
+  const resp = await getWeeklyProgSalesFfpds(startWeek[0].formatted_date_start, req.body.end, req.body.program)
 
   console.log(
-    `get get weekly sales for ${req.body.options.program} by by fresh/frozen, processed/dry, size during ${req.body.start} through ${req.body.end} route COMPLETE. \n`
+    `get get weekly sales for ${req.body.program} by by fresh/frozen, processed/dry, size during ${req.body.start} through ${req.body.end} route COMPLETE. \n`
   )
   res.send(resp)
 })
