fc165763d5317343c78f2662a53cbf0c2fdd4498
Eastern Dev <dev@seaflow.fish>
2023-06-07 08:05:38 -0400

    starting a new report for fresh/frozen, processed/dry, size

--- queries/postgres/getInven/forProgFfpds/getFgInven.js
+++ queries/postgres/getInven/forProgFfpds/getFgInven.js
@@ -1,8 +1,8 @@
-/* *********************************************** FG PROGRAM SALES *********************************************** */
+/* *********************************************** FG PROCESSING LEVEL SALES *********************************************** */
 
 // FG on hand grouped by program (includes in transit)
 
-const getFgByProgram = async () => {
+const getFgByProcessingLevel = async program => {
   try {
     const { Client } = require('pg')
     const pgClient = new Client() // config from ENV
@@ -11,8 +11,8 @@ const getFgByProgram = async () => {
     console.log(`query postgres for FG on hand ...`)
 
     const response = await pgClient.query(
-      'SELECT \'FG INVEN\' AS column, master_supplement.species_group AS maj_row, master_supplement.program AS min_row, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS cogs FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) GROUP BY master_supplement.species_group, master_supplement.program',
-      ['FG']
+      'SELECT \'FG INVEN\' AS column, master_supplement.fg_fresh_frozen AS maj_row, master_supplement.fg_treatment AS min_row, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS cogs FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND master_supplement.program = $2 GROUP BY master_supplement.fg_fresh_frozen, master_supplement.fg_treatment',
+      ['FG', program]
     ) //prettier-ignore
 
     await pgClient.end()
@@ -220,7 +220,7 @@ const getFgAtLocationTotal = async () => {
   }
 }
 
-module.exports.getFgByProgram = getFgByProgram
+module.exports.getFgByProcessingLevel = getFgByProcessingLevel
 module.exports.getFgInTransitByProgram = getFgInTransitByProgram
 module.exports.getFgAtLocationByProgram = getFgAtLocationByProgram
 module.exports.getFgByFreshFrozen = getFgByFreshFrozen
