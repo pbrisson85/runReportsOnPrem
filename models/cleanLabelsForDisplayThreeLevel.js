552d5cf57705eb391c5103892625432c7cf33163
Eastern Dev <dev@seaflow.fish>
2023-06-08 13:01:18 -0400

    starting a new report for fresh/frozen, processed/dry, size

--- models/cleanLabelsForDisplayThreeLevel.js
+++ models/cleanLabelsForDisplayThreeLevel.js
@@ -1,12 +1,65 @@
 const cleanLabelsForDisplay = flattenedMappedData => {
-  let subTotalFound = false
+  let l1Value = ''
+  let l2Value = ''
+
   flattenedMappedData.forEach((row, idx) => {
-    subTotalFound = testL1Subtotal(row, idx, subTotalFound, flattenedMappedData)
-    subTotalFound = testL2Subtotal(row, idx, subTotalFound, flattenedMappedData)
+    // Set initial values
+    if (idx === 0) {
+      l1Value = row.l1_grouping
+      l2Value = row.l2_grouping
+
+      return
+    }
+
+    // If l1 grouping does not change then don't show it
+    if (row.l1_grouping === l1Value) {
+      flattenedMappedData[idx].l2_grouping = ''
+    }
+
+    // If l2 grouping does not change then don't show it
+    if (row.l2_grouping === l2Value) {
+      flattenedMappedData[idx].l2_grouping = ''
+    }
+
+    // If l2 grouping includes subtotal then update the labels
+    if (row.l2_grouping.toUpperCase().includes('SUBTOTAL')) {
+      flattenedMappedData[idx].l1_grouping = `${flattenedMappedData[idx].l1_grouping} SUBTOTAL`
+      flattenedMappedData[idx].l2_grouping = ''
+      flattenedMappedData[idx].l3_grouping = ''
+    }
+
+    // If l3 grouping includes subtotal then update the labels
+    if (row.l3_grouping.toUpperCase().includes('SUBTOTAL')) {
+      flattenedMappedData[idx].l1_grouping = ''
+      flattenedMappedData[idx].l2_grouping = `${flattenedMappedData[idx].l2_grouping} SUBTOTAL`
+      flattenedMappedData[idx].l3_grouping = ''
+    }
+
+    // if l1 grouping does change, set new value
+    if (row.l1_grouping !== l1Value) {
+      l1Value = row.l1_grouping
+    }
+
+    // if l2 grouping does change, set new value
+    if (row.l2_grouping !== l2Value) {
+      l2Value = row.l2_grouping
+    }
   })
 
   return flattenedMappedData
 }
+
+// RE-WRITING THIS ALGO
+
+// const cleanLabelsForDisplay = flattenedMappedData => {
+//   let subTotalFound = false
+//   flattenedMappedData.forEach((row, idx) => {
+//     subTotalFound = testL1Subtotal(row, idx, subTotalFound, flattenedMappedData)
+//     subTotalFound = testL2Subtotal(row, idx, subTotalFound, flattenedMappedData)
+//   })
+
+//   return flattenedMappedData
+// }
 
 const testL1Subtotal = (row, idx, subTotalFound, flattenedMappedData) => {
   console.log('entering testL1Subtotal and subTotalFound is :', subTotalFound)
