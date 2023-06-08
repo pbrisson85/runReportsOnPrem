172290651f4280b82faa185fa94b64b692e1102c
Eastern Dev <dev@seaflow.fish>
2023-06-08 15:11:27 -0400

    starting a new report for fresh/frozen, processed/dry, size

--- queries/postgres/getInven/forProgFfpds/getFgInven.js
+++ queries/postgres/getInven/forProgFfpds/getFgInven.js
@@ -213,7 +213,7 @@ const getFgInTransitTotal = async program => {
     console.log(`query postgres for FG on hand ...`)
 
     const response = await pgClient.query(
-      'SELECT \'FG INVEN\' AS column, \'FG SALES\' AS l1_grouping, \'TOTAL\' AS l2_grouping, \'TOTAL\' AS l3_grouping, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS cogs FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND master_supplement.program = $2 AND perpetual_inventory.location_type = $3',
+      'SELECT \'FG IN TRANSIT\' AS column, \'FG SALES\' AS l1_grouping, \'TOTAL\' AS l2_grouping, \'TOTAL\' AS l3_grouping, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS cogs FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND master_supplement.program = $2 AND perpetual_inventory.location_type = $3',
       ['FG', program, 'IN TRANSIT']
     ) //prettier-ignore
 
@@ -237,7 +237,7 @@ const getFgAtLocationTotal = async program => {
     console.log(`query postgres for FG on hand ...`)
 
     const response = await pgClient.query(
-      'SELECT \'FG INVEN\' AS column, \'FG SALES\' AS l1_grouping, \'TOTAL\' AS l2_grouping, \'TOTAL\' AS l3_grouping, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS cogs FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND master_supplement.program = $2 AND perpetual_inventory.location_type <> $3',
+      'SELECT \'FG ON HAND\' AS column, \'FG SALES\' AS l1_grouping, \'TOTAL\' AS l2_grouping, \'TOTAL\' AS l3_grouping, SUM(perpetual_inventory.on_hand_lbs) AS lbs, SUM(perpetual_inventory.cost_extended) AS cogs FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement ON master_supplement.item_num = perpetual_inventory.item_number WHERE master_supplement.byproduct_type IS NULL AND master_supplement.item_type = $1 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND master_supplement.program = $2 AND perpetual_inventory.location_type <> $3',
       ['FG', program, 'IN TRANSIT']
     ) //prettier-ignore
 
