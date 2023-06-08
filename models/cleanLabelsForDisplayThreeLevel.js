5e1741d44abb72c747af437d7b79a649db450b59
Eastern Dev <dev@seaflow.fish>
2023-06-08 13:18:30 -0400

    starting a new report for fresh/frozen, processed/dry, size

--- models/cleanLabelsForDisplayThreeLevel.js
+++ models/cleanLabelsForDisplayThreeLevel.js
@@ -15,58 +15,37 @@ const cleanLabelsForDisplay = flattenedMappedData => {
       return
     }
 
-    console.log('idx: ', idx)
-    console.log('Labels: ', row.l1_grouping, row.l2_grouping, row.l3_grouping)
-
     // If l1 grouping does not change then don't show it
     if (row.l1_grouping === l1Value) {
-      console.log('l1 grouping does not change... erasing value')
-
       flattenedMappedData[idx].l1_grouping = ''
     }
 
     // If l2 grouping does not change then don't show it
     if (row.l2_grouping === l2Value) {
-      console.log('l2 grouping does not change... erasing value')
-
       flattenedMappedData[idx].l2_grouping = ''
     }
 
     // If l2 grouping includes subtotal then update the labels
     if (row.l2_grouping.toUpperCase().includes('SUBTOTAL')) {
-      console.log('l1 is a subtotal, setting values...')
-
       flattenedMappedData[idx].l1_grouping = `${row.l1_grouping} SUBTOTAL`
       flattenedMappedData[idx].l2_grouping = ''
       flattenedMappedData[idx].l3_grouping = ''
-
-      console.log('was: ', row.l1_grouping, row.l2_grouping, row.l3_grouping)
-      console.log('now: ', flattenedMappedData[idx].l1_grouping, flattenedMappedData[idx].l2_grouping, flattenedMappedData[idx].l3_grouping)
     }
 
     // If l3 grouping includes subtotal then update the labels
-    if (row.l3_grouping.toUpperCase().includes('SUBTOTAL')) {
-      console.log('l2 is a subtotal, setting values...')
-
+    if (row.l3_grouping.toUpperCase().includes('SUBTOTAL') && !row.l2_grouping.toUpperCase().includes('SUBTOTAL')) {
       flattenedMappedData[idx].l1_grouping = ''
       flattenedMappedData[idx].l2_grouping = `${row.l2_grouping} SUBTOTAL`
       flattenedMappedData[idx].l3_grouping = ''
-
-      console.log('was: ', row.l1_grouping, row.l2_grouping, row.l3_grouping)
-      console.log('now: ', flattenedMappedData[idx].l1_grouping, flattenedMappedData[idx].l2_grouping, flattenedMappedData[idx].l3_grouping)
     }
 
     // if l1 grouping does change, set new value
     if (row.l1_grouping !== l1Value) {
-      console.log('l1 grouping does change... setting new value to ', row.l1_grouping)
-
       l1Value = row.l1_grouping
     }
 
     // if l2 grouping does change, set new value
     if (row.l2_grouping !== l2Value) {
-      console.log('l2 grouping does change... setting new value to ', row.l2_grouping)
-
       l2Value = row.l2_grouping
     }
   })
