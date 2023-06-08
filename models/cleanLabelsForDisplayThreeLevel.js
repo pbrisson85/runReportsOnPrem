f5e95f009144f2ad203411bd64b3ddb15ebcda7d
Eastern Dev <dev@seaflow.fish>
2023-06-08 08:11:42 -0400

    starting a new report for fresh/frozen, processed/dry, size

--- models/cleanLabelsForDisplayThreeLevel.js
+++ models/cleanLabelsForDisplayThreeLevel.js
@@ -1,8 +1,8 @@
 const cleanLabelsForDisplay = flattenedMappedData => {
   let subTotalFound = false
   flattenedMappedData.forEach((row, idx) => {
-    testL1Subtotal(row, idx, subTotalFound, flattenedMappedData)
-    testL2Subtotal(row, idx, subTotalFound, flattenedMappedData)
+    subTotalFound = testL1Subtotal(row, idx, subTotalFound, flattenedMappedData)
+    subTotalFound = testL2Subtotal(row, idx, subTotalFound, flattenedMappedData)
   })
 
   return flattenedMappedData
@@ -10,7 +10,7 @@ const cleanLabelsForDisplay = flattenedMappedData => {
 
 const testL1Subtotal = (row, idx, subTotalFound, flattenedMappedData) => {
   // leave first row alone
-  if (idx === 0) return row
+  if (idx === 0) return subTotalFound
 
   // subtotal hit flip flag true but return hidden species flag
   if (row.l2_grouping.toUpperCase().includes('TOTAL')) {
@@ -20,20 +20,23 @@ const testL1Subtotal = (row, idx, subTotalFound, flattenedMappedData) => {
     flattenedMappedData[idx].l1_grouping = `${flattenedMappedData[idx].l1_grouping} SUBTOTAL`
     flattenedMappedData[idx].l2_grouping = ''
     flattenedMappedData[idx].l3_grouping = ''
+    return subTotalFound
   }
 
   // If row is NOT an l1 subtotal, remove l1 label
   if (!subTotalFound) {
     flattenedMappedData[idx].l1_grouping = ''
+    return subTotalFound
   }
 
   // not first row, not subtotal, but a subtotal has been found. This is the first row after the subtotal. flip flag false for next row and return the species for this row
   subTotalFound = false
+  return subTotalFound
 }
 
 const testL2Subtotal = (row, idx, subTotalFound, flattenedMappedData) => {
   // leave first row alone
-  if (idx === 0) return row
+  if (idx === 0) return subTotalFound
 
   // subtotal hit flip flag true but return hidden species flag
   if (row.l3_grouping.toUpperCase().includes('TOTAL')) {
@@ -43,15 +46,18 @@ const testL2Subtotal = (row, idx, subTotalFound, flattenedMappedData) => {
     flattenedMappedData[idx].l1_grouping = ''
     flattenedMappedData[idx].l2_grouping = `${flattenedMappedData[idx].l2_grouping} SUBTOTAL`
     flattenedMappedData[idx].l3_grouping = ''
+    return subTotalFound
   }
 
   // If row is NOT an l1 subtotal, remove l1 label
   if (!subTotalFound) {
     flattenedMappedData[idx].l2_grouping = ''
+    return subTotalFound
   }
 
   // not first row, not subtotal, but a subtotal has been found. This is the first row after the subtotal. flip flag false for next row and return the species for this row
   subTotalFound = false
+  return subTotalFound
 }
 
 module.exports = cleanLabelsForDisplay
