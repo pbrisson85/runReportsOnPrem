70c01c82776ed5b4f9347938365f1360ca5eb6b4
Eastern Dev <dev@seaflow.fish>
2023-06-13 19:46:05 -0400

    adding tagged untagged on hand to two level report

--- reports/forProgSpecSoakSize/queries/hardcode/cols.js
+++ reports/forProgSpecSoakSize/queries/hardcode/cols.js
@@ -1,7 +1,7 @@
 const cols = [
   {
     unfilteredColIdx: 0, // index of col in original data GETS RENUMBERED ON INITIALIZATION OF SELECTOR
-    displayName: 'FRESH/FROZEN', // show as column header
+    displayName: 'SPECIES', // show as column header
     dataName: 'l1_subtotal', // key to pull data from
     justifyData: 'start', // css justify content
     justifyHeading: 'center', // css justify content
