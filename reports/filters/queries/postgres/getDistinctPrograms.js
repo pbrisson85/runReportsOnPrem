cc66082170166e20c2d69e6fe44fa1f6a1b507bb
Eastern Dev <dev@seaflow.fish>
2023-06-15 19:05:57 -0400

    cleaned up routing so all four current reports are accesible. also added where clause to proframs filter so only finished goods programs are shown

--- reports/filters/queries/postgres/getDistinctPrograms.js
+++ reports/filters/queries/postgres/getDistinctPrograms.js
@@ -6,9 +6,15 @@ const getDistinctPrograms = async fy => {
 
     console.log(`query postgres to get list of programs for filters ...`)
 
+    /*
+    Original query:
+
+    'SELECT DISTINCT(TRIM(ms.program)) AS label, (TRIM(ms.program)) AS "dataName" FROM "salesReporting".sales_line_items LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sales_line_items.item_number WHERE sales_line_items.fiscal_year = $1'
+    */
+
     const response = await pgClient.query(
-      'SELECT DISTINCT(TRIM(ms.program)) AS label, (TRIM(ms.program)) AS "dataName" FROM "salesReporting".sales_line_items LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sales_line_items.item_number WHERE sales_line_items.fiscal_year = $1',
-      [fy]
+      'SELECT DISTINCT(TRIM(ms.program)) AS label, (TRIM(ms.program)) AS "dataName" FROM "salesReporting".sales_line_items LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sales_line_items.item_number WHERE ms.byproduct_type IS NULL AND ms.item_type = $2 sales_line_items.fiscal_year = $1 UNION SELECT DISTINCT(TRIM(ms.program)) AS label, (TRIM(ms.program)) AS "dataName" FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = perpetual_inventory.item_number WHERE ms.byproduct_type IS NULL AND ms.item_type = $2 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) UNION SELECT DISTINCT(TRIM(ms.program)) AS label, (TRIM(ms.program)) AS "dataName" FROM "salesReporting".sales_orders LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sales_orders.item_num WHERE ms.byproduct_type IS NULL AND ms.item_type = $2 AND sales_orders.version = (SELECT MAX(sales_orders.version) - 1 FROM "salesReporting".sales_orders)',
+      [fy, 'FG']
     )
 
     await pgClient.end()
--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const app = express()
 const helmet = require('helmet')
 
 // initialize routes
+/* Reports */
 const getSalesByProgram = require('./reports/bySpeciesgroupProg/routes/getWklyReport')
-const getSalesFrzSoakSize = require('./reports/forProgSpecBrndSize/routes/getWklyReport')
+const getSalesFrzSoakSize = require('./reports/forProgBySpecBrndSize/routes/getWklyReport')
+const getSalesSpecBrndSize = require('./reports/forProgBySpecBrndSize/routes/getWklyReport')
+const getSalesSpecSoakSize = require('./reports/forProgBySpecSoakSize/routes/getWklyReport')
+/* Data */
 const generateSalesData = require('./generateSales/routes/generateSales')
+/* UI */
 const getFilters = require('./reports/filters/routes/getFilters')
 
 // initialize startup procedures
@@ -31,8 +36,10 @@ process.on('unhandledRejection', ex => {
 // routes
 app.use(helmet())
 app.use(express.json())
-app.use('/api/sales/byProgram', getSalesByProgram)
-app.use('/api/sales/forProgramFfpds', getSalesFrzSoakSize)
+app.use('/api/sales/bySpeciesgroupProg', getSalesByProgram)
+app.use('/api/sales/forProgram/byFrzSoakSize', getSalesFrzSoakSize)
+app.use('/api/sales/forProgram/bySpecBrndSize', getSalesSpecBrndSize)
+app.use('/api/sales/forProgram/bySpecSoakSize', getSalesSpecSoakSize)
 app.use('/api/sales/generateSalesData', generateSalesData)
 app.use('/api/sales/getFilters', getFilters)
 
