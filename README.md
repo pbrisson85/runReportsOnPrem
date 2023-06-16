f00268ba4c1c215183cf7b29fd09a10b52e3de6e
Eastern Dev <dev@seaflow.fish>
2023-06-16 06:52:31 -0400

    added route for reports filter

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ### TO DO
 
 - Add non FG sales so report can be tied into seasoft
+- ADD MORE REPORTS AND KEEP WORJKING ON DEFAULTS
+- MAKE SALES TREND VIEWABLE BY CALENDAR MONTH, FISCAL PERIOD, CALENDAR QUARTER, CALENDAR YEAR, FISCAL YEAR
+- MAKE ALL DRILL DOWNS
 
 - FrzSoakSize - sort the size correctly
 - FrzSoakSize - add detail routes
--- reports/filters/queries/hardcode/getReportsFilters.js
+++ reports/filters/queries/hardcode/getReportsFilters.js
@@ -3,22 +3,27 @@ const getReportsFilters = () => {
     {
       label: 'species group / program',
       dataName: 'speciesgroupProg',
-      default: true,
+      defaults: ['all'], // list of programs (datanames) that this is the default report
+      optional: ['all'], // list of programs (datanames) that allow this
     },
     {
       label: 'freeze / processing / size',
       dataName: 'frzSoakSize',
-      default: false,
+      default: true, // if true, this is the default report for an unlisted program (one which does not appear in the defaults array)
+      defaults: ['COD USA', 'FLATFISH USA', 'HADDOCK USA', 'PERCH USA', 'POLLOCK USA', 'SCALLOPS DOMESTIC'], // list of programs (datanames) that this is the default report
+      optional: ['any'], // list of programs (datanames) that allow this, or use 'any' to allow all programs EXCEPT 'all'
     },
     {
       label: 'species / brand / size',
       dataName: 'specBrndSize',
-      default: false,
+      defaults: ['COD CHN', 'FLATFISH CHN', 'FLATFISH PER', 'HADDOCK CHN', 'PERCH CHN', 'POLLOCK CHN', 'SEAFOOD OTHER'],
+      optional: ['any'],
     },
     {
       label: 'species / soak / size',
-      dataName: 'bySpecSoakSize',
-      default: false,
+      dataName: 'specSoakSize',
+      defaults: ['SCALLOPS NON DOMESTIC'],
+      optional: ['any'],
     },
   ]
 }
