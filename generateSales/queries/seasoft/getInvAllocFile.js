75f7497f02294700abdb724a6e09749187adc8f0
Eastern Dev <dev@seaflow.fish>
2023-06-17 14:45:17 -0400

    adding routine for saving the invoice allocations file

--- generateSales/queries/seasoft/getInvAllocFile.js
+++ generateSales/queries/seasoft/getInvAllocFile.js
@@ -6,7 +6,7 @@ const getInvAllocFile = async (firstDayOfFy, firstDayOfNextFy) => {
 
     console.log(`query ODBC for Invoice Allocation File for dates ${firstDayOfFy} to ${firstDayOfNextFy} ...`)
 
-    const queryString = "SELECT \"Invoice Allocations\".INVOICE_DATE, {fn RTRIM(\"Invoice Allocations\".INVOICE_NUMBER)}, {fn RTRIM(\"Invoice Allocations\".INVOICE_LINE_NUMBER)}, {fn RTRIM(\"Invoice Allocations\".PRODUCT_NUMBER)}, \"Invoice Allocations\".EXPENSE_AMOUNT, {fn RTRIM(\"Invoice Allocations\".EXPENSE_CODE}) FROM 'Invoice Allocations' WHERE \"Invoice Allocations\".EXPENSE_CODE <> 'SALE' AND \"Invoice Allocations\".INVOICE_DATE >= ? AND \"Invoice Allocations\".INVOICE_DATE < ?" //prettier-ignore
+    const queryString = "SELECT \"Invoice Allocations\".INVOICE_DATE, {fn RTRIM(\"Invoice Allocations\".INVOICE_NUMBER)} AS INVOICE_NUMBER, {fn RTRIM(\"Invoice Allocations\".INVOICE_LINE_NUMBER)} AS INVOICE_LINE_NUMBER, {fn RTRIM(\"Invoice Allocations\".PRODUCT_NUMBER)} AS PRODUCT_NUMBER, \"Invoice Allocations\".EXPENSE_AMOUNT, {fn RTRIM(\"Invoice Allocations\".EXPENSE_CODE}) AS EXPENSE_CODE FROM 'Invoice Allocations' WHERE \"Invoice Allocations\".EXPENSE_CODE <> 'SALE' AND \"Invoice Allocations\".INVOICE_DATE >= ? AND \"Invoice Allocations\".INVOICE_DATE < ?" //prettier-ignore
 
     const data = await odbcConn.query(queryString, [firstDayOfFy, firstDayOfNextFy])
 
