b14a2ea8ec0dcc2ecbaa53932a99d16d88bd2b95
Eastern Dev <dev@seaflow.fish>
2023-06-17 14:29:57 -0400

    adding routine for saving the invoice allocations file

--- generateSales/queries/seasoft/getInvAllocFile.js
+++ generateSales/queries/seasoft/getInvAllocFile.js
@@ -6,7 +6,7 @@ const getInvAllocFile = async (firstDayOfFy, firstDayOfNextFy) => {
 
     console.log(`query ODBC for Invoice Allocation File for dates ${firstDayOfFy} to ${firstDayOfNextFy} ...`)
 
-    const queryString = "SELECT \"Invoice Allocations\".INVOICE_DATE, \"Invoice Allocations\".INVOICE_NUMBER, \"Invoice Allocations\".INVOICE_LINE_NUMBER, \"Invoice Allocations\".PRODUCT_NUMBER, \"Invoice Allocations\".EXPENSE_AMOUNT, \"Invoice Allocations\".EXPENSE_CODE, \"General Table File\".TABLE_FLD02 FROM {OJ 'Invoice Allocations' LEFT OUTER JOIN 'General Table File' ON \"Invoice Allocations\".EXPENSE_CODE = \"General Table File\".TABLE_CODE} WHERE \"Invoice Allocations\".EXPENSE_CODE <> 'SALE' AND \"General Table File\".TABLE_ID = 'OTHP' AND \"Invoice Allocations\".INVOICE_DATE >= ? AND \"Invoice Allocations\".INVOICE_DATE < ?" //prettier-ignore
+    const queryString = "SELECT \"Invoice Allocations\".INVOICE_DATE, \"Invoice Allocations\".INVOICE_NUMBER, \"Invoice Allocations\".INVOICE_LINE_NUMBER, \"Invoice Allocations\".PRODUCT_NUMBER, \"Invoice Allocations\".EXPENSE_AMOUNT, \"Invoice Allocations\".EXPENSE_CODE FROM 'Invoice Allocations' WHERE \"Invoice Allocations\".EXPENSE_CODE <> 'SALE' AND \"Invoice Allocations\".INVOICE_DATE >= ? AND \"Invoice Allocations\".INVOICE_DATE < ?" //prettier-ignore
 
     const data = await odbcConn.query(queryString, [firstDayOfFy, firstDayOfNextFy])
 
@@ -19,3 +19,7 @@ const getInvAllocFile = async (firstDayOfFy, firstDayOfNextFy) => {
 }
 
 module.exports = getInvAllocFile
+
+/*
+"SELECT \"Invoice Allocations\".INVOICE_DATE, \"Invoice Allocations\".INVOICE_NUMBER, \"Invoice Allocations\".INVOICE_LINE_NUMBER, \"Invoice Allocations\".PRODUCT_NUMBER, \"Invoice Allocations\".EXPENSE_AMOUNT, \"Invoice Allocations\".EXPENSE_CODE, \"General Table File\".TABLE_FLD02 FROM {OJ 'Invoice Allocations' LEFT OUTER JOIN 'General Table File' ON \"Invoice Allocations\".EXPENSE_CODE = \"General Table File\".TABLE_CODE} WHERE \"Invoice Allocations\".EXPENSE_CODE <> 'SALE' AND \"General Table File\".TABLE_ID = 'OTHP' AND \"Invoice Allocations\".INVOICE_DATE >= ? AND \"Invoice Allocations\".INVOICE_DATE < ?"
+*/
