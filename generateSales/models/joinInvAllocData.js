58d2ed48ab3f837e2f94de626780bb5ffe61668c
Eastern Dev <dev@seaflow.fish>
2023-06-17 15:40:10 -0400

    adding routine for saving the invoice allocations file

--- generateSales/models/joinInvAllocData.js
+++ generateSales/models/joinInvAllocData.js
@@ -1,6 +1,10 @@
 const joinInvAllocData = (inAllocData, genTblOthp_unflat) => {
   const mapped = inAllocData.map(row => {
-    const contra = genTblOthp_unflat[row.EXPENSE_CODE][0]?.GL_CODE
+    let contra = null
+
+    if (typeof genTblOthp_unflat[row.EXPENSE_CODE] !== 'undefined') {
+      contra = genTblOthp_unflat[row.EXPENSE_CODE][0].GL_CODE
+    }
 
     return {
       ...row,
