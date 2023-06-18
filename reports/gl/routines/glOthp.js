a8f00d1849426449f7809a7436cf177678b568d3
Eastern Dev <dev@seaflow.fish>
2023-06-18 10:18:54 -0400

    adding gl tie out reports

--- reports/gl/routines/glOthp.js
+++ reports/gl/routines/glOthp.js
@@ -20,6 +20,8 @@ const glOthp = async fy => {
 
   // map the othp type into the othp gl data
   const mapped = othpGl.map(othp => {
+    console.log(othp)
+
     const othpType = contraSalesGlMap_unflat[othp.othp_gl][0].category
 
     return {
