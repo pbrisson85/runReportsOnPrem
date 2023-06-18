2652382630aba8050e201f7c291b84b33554f232
Eastern Dev <dev@seaflow.fish>
2023-06-18 10:16:38 -0400

    adding gl tie out reports

--- reports/gl/routines/glOthp.js
+++ reports/gl/routines/glOthp.js
@@ -1,16 +1,32 @@
 const calcOthpGl = require('../queries/postgres/calcOthpGl')
 const getContraSalesGlMap = require('../queries/postgres/getContraSalesGlMap')
 const getMajCodeGlMap = require('../queries/postgres/getMajCodeGlMap')
+const unflattenByCompositKey = require('../../shared/models/unflattenByCompositKey')
 
 const glOthp = async fy => {
   // Make DB Call
   console.log(`Calc OTHP for FY:${fy} routine... \n`)
 
+  /* QUERY DATA */
   const othpGl = await calcOthpGl(fy)
   const contraSalesGlMap = await getContraSalesGlMap()
   const majCodeGlMap = await getMajCodeGlMap()
 
-  return { contraSalesGlMap, majCodeGlMap, othpGl }
+  /* MODEL DATA */
+  // create othp allocation
+  const contraSalesGlMap_unflat = unflattenByCompositKey(contraSalesGlMap, { 1: 'contra' })
+
+  // map the othp type into the othp gl data
+  const mapped = othpGl.map(othp => {
+    const othpType = contraSalesGlMap_unflat[othp.othp_gl].category
+
+    return {
+      ...othp,
+      othpType,
+    }
+  })
+
+  return { mapped }
 }
 
 module.exports = glOthp
