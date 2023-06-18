18d690862f0e04f5f89407c021b8b7149e7ff620
Eastern Dev <dev@seaflow.fish>
2023-06-18 10:58:15 -0400

    adding gl tie out reports

--- reports/gl/queries/seasoft/getGlPeriodActivity.js
+++ reports/gl/queries/seasoft/getGlPeriodActivity.js
@@ -1,4 +1,4 @@
-const { createConnection } = require('../../../database/seasoftODBC')
+const { createConnection } = require('../../../../database/seasoftODBC')
 
 const getGlPeriodActivity = async fy => {
   try {
