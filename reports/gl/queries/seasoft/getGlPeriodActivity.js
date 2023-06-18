b7106c068dbeb530e5a156bb0d05421ad051b86a
Eastern Dev <dev@seaflow.fish>
2023-06-18 10:57:32 -0400

    adding gl tie out reports

---
+++ reports/gl/queries/seasoft/getGlPeriodActivity.js
@@ -0,0 +1,21 @@
+const { createConnection } = require('../../../database/seasoftODBC')
+
+const getGlPeriodActivity = async fy => {
+  try {
+    const odbcConn = await createConnection()
+
+    console.log(`query ODBC for General Table OTHP ...`)
+
+    const queryString = "SELECT \"GL Period Activity\".ACCOUNT_NUMBER, \"GL Period Activity\".DEPARTMENT_CODE, \"GL Period Activity\".NET_AMOUNTS_1 AS P1, \"GL Period Activity\".NET_AMOUNTS_2 AS P2, \"GL Period Activity\".NET_AMOUNTS_3 AS P3, \"GL Period Activity\".NET_AMOUNTS_4 AS P4, \"GL Period Activity\".NET_AMOUNTS_5 AS P5, \"GL Period Activity\".NET_AMOUNTS_6 AS P6, \"GL Period Activity\".NET_AMOUNTS_7 AS P7, \"GL Period Activity\".NET_AMOUNTS_8 AS P8, \"GL Period Activity\".NET_AMOUNTS_9 AS P9, \"GL Period Activity\".NET_AMOUNTS_10 AS P10, \"GL Period Activity\".NET_AMOUNTS_11 AS P11, \"GL Period Activity\".NET_AMOUNTS_12 AS P12 FROM 'GL Period Activity' WHERE \"GL Period Activity\".COMPANY_NUMBER = ? AND \"GL Period Activity\".FISCAL_YEAR = ?" //prettier-ignore
+
+    const data = await odbcConn.query(queryString, ['0001', fy])
+
+    await odbcConn.close()
+
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = getGlPeriodActivity
--- reports/gl/routines/glOthp.js
+++ reports/gl/routines/glOthp.js
@@ -2,6 +2,8 @@ const calcOthpGl = require('../queries/postgres/calcOthpGl')
 const getContraSalesGlMap = require('../queries/postgres/getContraSalesGlMap')
 const getMajCodeGlMap = require('../queries/postgres/getMajCodeGlMap')
 const unflattenByCompositKey = require('../../shared/models/unflattenByCompositKey')
+const getGlPeriodActivity = require('../queries/seasoft/getGlPeriodActivity')
+const mapOthpGlRecalc = require('../models/mapOthpGlRecalc')
 
 const glOthp = async fy => {
   // Make DB Call
@@ -11,39 +13,17 @@ const glOthp = async fy => {
   const othpGl = await calcOthpGl(fy)
   const contraSalesGlMap = await getContraSalesGlMap()
   const majCodeGlMap = await getMajCodeGlMap()
+  const glPeriodActivity = await getGlPeriodActivity(fy)
 
   /* MODEL DATA */
   // create othp allocation
   const contraSalesGlMap_unflat = unflattenByCompositKey(contraSalesGlMap, { 1: 'contra' })
   const majCodeGlMap_unflat = unflattenByCompositKey(majCodeGlMap, { 1: 'name' })
+  const mappedOthpGl = mapOthpGlRecalc(othpGl, contraSalesGlMap_unflat, majCodeGlMap_unflat)
 
   // map the othp type into the othp gl data
-  const mapped = othpGl.map(othp => {
-    // Find OTHP type (rebate, commission, discount, outbound)
-    let othpType = 'OTHER' // example: OTHP mapped to 9010 extended terms
-    let allocationGl = '9999' // example: OTHP mapped to 9010 extended terms
 
-    if (typeof contraSalesGlMap_unflat[othp.othp_gl] !== 'undefined') {
-      othpType = contraSalesGlMap_unflat[othp.othp_gl].category
-      allocationGl = contraSalesGlMap_unflat[othp.othp_gl].allocation
-    }
-
-    // Find OTHP allocation GL accounts
-    let allocattedGl = '9999' // example: OTHP mapped to 9010 extended terms
-
-    if (typeof majCodeGlMap_unflat[othp.major_code_name] !== 'undefined') {
-      allocattedGl = majCodeGlMap_unflat[othp.major_code_name][othpType]
-    }
-
-    return {
-      ...othp,
-      othpType,
-      allocationGl,
-      allocattedGl,
-    }
-  })
-
-  return { mapped }
+  return { glPeriodActivity, mappedOthpGl }
 }
 
 module.exports = glOthp
