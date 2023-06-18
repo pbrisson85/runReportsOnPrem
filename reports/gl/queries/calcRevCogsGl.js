1b0dba92cfde872241e9282f4533c1fdf96e0dbb
Eastern Dev <dev@seaflow.fish>
2023-06-18 10:00:22 -0400

    adding gl tie out reports

--- reports/gl/queries/calcRevCogsGl.js
+++ reports/gl/queries/calcRevCogsGl.js
@@ -9,7 +9,7 @@ const calcRevCogsGl = async fy => {
     // level 1 detail
 
     const response = await pgClient.query(
-          'SELECT i.major_code_name, s.period, gl.dept as dept_gl, gl.sales AS sales_gl, gl.cogs AS cogs_gl, ROUND(SUM(s.calc_gl_gross_sales)::numeric,2) AS sales, ROUND(SUM(s.calc_gl_cogs)::numeric,2) AS cogs, ROUND(SUM(s.calc_gm_rept_weight)::numeric,2) AS lbs FROM "salesReporting".sales_line_items AS sLEFT OUTER JOIN "invenReporting".master_supplement as i ON i.item_num = s.item_number LEFT OUTER JOIN "salesReporting".maj_code_gl_map as gl ON gl.name = i.major_code_name WHERE s.fiscal_year = $1 GROUP BY s.period, i.major_code_name, gl.dept, gl.sales, gl.cogs',
+          'SELECT i.major_code_name, s.period, gl.dept as dept_gl, gl.sales AS sales_gl, gl.cogs AS cogs_gl, ROUND(SUM(s.calc_gl_gross_sales)::numeric,2) AS sales, ROUND(SUM(s.calc_gl_cogs)::numeric,2) AS cogs, ROUND(SUM(s.calc_gm_rept_weight)::numeric,2) AS lbs FROM "salesReporting".sales_line_items AS s LEFT OUTER JOIN "invenReporting".master_supplement as i ON i.item_num = s.item_number LEFT OUTER JOIN "salesReporting".maj_code_gl_map as gl ON gl.name = i.major_code_name WHERE s.fiscal_year = $1 GROUP BY s.period, i.major_code_name, gl.dept, gl.sales, gl.cogs',
           [fy]
         ) //prettier-ignore
 
