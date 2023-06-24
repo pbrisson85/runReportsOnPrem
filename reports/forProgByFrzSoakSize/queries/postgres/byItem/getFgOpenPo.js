96de533dc8687bce90e6cc6d36789c125208de7d
Eastern Dev <dev@seaflow.fish>
2023-06-24 17:56:47 -0400

    adding drilldown and detail

--- reports/forProgByFrzSoakSize/queries/postgres/byItem/getFgOpenPo.js
+++ reports/forProgByFrzSoakSize/queries/postgres/byItem/getFgOpenPo.js
@@ -21,7 +21,7 @@ const lvl_1_subtotal_getFgPo = async (program, filters) => {
     console.log(`level 1: query postgres for FG open PO ...`)
 
     const response = await pgClient.query(
-         'SELECT \'FG ON ORDER\' AS column, ms.item_num AS l1_label, COALESCE(SUM(perpetual_inventory.on_order_lbs),0) AS lbs, COALESCE(SUM(perpetual_inventory.on_order_extended),0) AS cogs FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = perpetual_inventory.item_number WHERE ms.item_type = $1 AND perpetual_inventory.on_order_lbs <> 0 AND perpetual_inventory.version = (SELECT MAX(version) - 1 FROM "invenReporting".perpetual_inventory) AND ms.program = $2 AND ms.fg_fresh_frozen = $3 AND ms.fg_treatment = $4 AND ms.size_name = $5 GROUP BY ms.item_num', ['FG', program, filters[0], filters[1], filters[2]]
+         'SELECT \'FG ON ORDER\' AS column, ms.item_num AS l1_label, ms.description AS l2_label, COALESCE(SUM(perpetual_inventory.on_order_lbs),0) AS lbs, COALESCE(SUM(perpetual_inventory.on_order_extended),0) AS cogs FROM "invenReporting".perpetual_inventory LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = perpetual_inventory.item_number WHERE ms.item_type = $1 AND perpetual_inventory.on_order_lbs <> 0 AND perpetual_inventory.version = (SELECT MAX(version) - 1 FROM "invenReporting".perpetual_inventory) AND ms.program = $2 AND ms.fg_fresh_frozen = $3 AND ms.fg_treatment = $4 AND ms.size_name = $5 GROUP BY ms.item_num, ms.description', ['FG', program, filters[0], filters[1], filters[2]]
         ) //prettier-ignore
 
     await pgClient.end()
