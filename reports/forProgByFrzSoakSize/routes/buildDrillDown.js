16ae26c10f8e8285b80f6596ba6632410852b50c
Eastern Dev <dev@seaflow.fish>
2023-06-24 17:03:55 -0400

    adding drilldown and detail

--- reports/forProgByFrzSoakSize/routes/buildDrillDown.js
+++ reports/forProgByFrzSoakSize/routes/buildDrillDown.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const buildDrillDown = require('../routines/buildDrillDown')
+const buildDrillDown_item = require('../routines/buildDrillDown_item')
 const { getStartOfWeek } = require('../../shared/queries/postgres/getDateStartByWeek')
 
 // @route   POST /api/sales/drillDown/forProgBySpecSoakSize
@@ -14,10 +14,16 @@ router.post('/', async (req, res) => {
   // Note that start date is the END of the first week. Need the beginning of the same week to pull invoice dates that are after this:
   const startWeek = await getStartOfWeek(periodStart)
 
-  const resp = await buildDrillDown(program, option, filters, columnDataName, reportName, colType, startWeek, periodEnd)
+  let response = null
+
+  if (option === 'item') {
+    response = await buildDrillDown_item(program, startWeek[0].formatted_date_start, periodEnd, filters)
+  } else {
+    return res.send('not yet implemented')
+  }
 
   console.log(`get drilldown data for ${reportName} route COMPLETE. \n`)
-  res.send(resp)
+  res.send(response)
 })
 
 module.exports = router
