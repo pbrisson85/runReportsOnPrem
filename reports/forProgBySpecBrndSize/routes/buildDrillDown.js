504af830bdcade72bea17e2a43ff57c071967e7f
Eastern Dev <dev@seaflow.fish>
2023-06-24 09:40:16 -0400

    adding drilldown routes. test.

---
+++ reports/forProgBySpecBrndSize/routes/buildDrillDown.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+
+// @route   POST /api/sales/drillDown/forProgBySpecBrndSize
+// @desc    Get drilldown data for a given report and filter
+// @access  Private
+
+router.post('/', async (req, res) => {
+  const { option, filters, columnDataName, reportName, colType, periodStart, periodEnd } = req.body
+
+  console.log(`\nget drilldown data for ${reportName} route HIT...`)
+
+  console.log('option', option)
+  console.log('filters', filters)
+  console.log('columnDataName', columnDataName)
+  console.log('reportName', reportName)
+  console.log('colType', colType)
+  console.log('periodStart', periodStart)
+  console.log('periodEnd', periodEnd)
+
+  res.send('ok')
+
+  //const resp = await runDrilldown(reportName, filterName, start, end, filter)
+
+  console.log(`get drilldown data for ${reportName} route COMPLETE. \n`)
+  //res.send(resp)
+})
+
+module.exports = router
