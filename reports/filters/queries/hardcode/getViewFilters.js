2eea557b21eba8655530cfc21f4048eaa1df2691
Eastern Dev <dev@seaflow.fish>
2023-06-28 14:08:46 -0400

    added datapoint to views filter

--- reports/filters/queries/hardcode/getViewFilters.js
+++ reports/filters/queries/hardcode/getViewFilters.js
@@ -4,56 +4,67 @@ const getViewFilters = () => {
       label: 'WEIGHT (lbs)',
       dataName: 'weight',
       additionalData: { decimals: 0 },
+      cols: ['label', 'trendCol', 'trendTotal', 'SO', 'InvenFG', 'InvenRM', 'PO'], // list of colTypes that should be displayed in this view
     },
     // {
     //   label: 'REVENUE $',
     //   dataName: 'revenue',
     //   additionalData: { decimals: 0 },
+    //   cols: ['label', 'trendCol', 'trendTotal', 'SO'], // No inven, only sales data
     // },
     // {
     //   label: 'OTHP $',
     //   dataName: 'othp',
     //   additionalData: { decimals: 0 },
+    //   cols: ['label', 'trendCol', 'trendTotal', 'SO'], // No inven, only sales data
     // },
     {
-      label: 'sales: NET REVENUE $, inven: COST $',
+      label: 'NET REVENUE',
       dataName: 'netSales',
       additionalData: { decimals: 0 },
+      cols: ['label', 'trendCol', 'trendTotal', 'SO'], // No inven, only sales data
     },
     {
-      label: 'COST $',
+      label: 'COST',
       dataName: 'cogs',
       additionalData: { decimals: 0 },
+      cols: ['label', 'trendCol', 'trendTotal', 'SO', 'InvenFG', 'InvenRM', 'PO'],
     },
     {
-      label: 'sales: GM $, inven: COST $',
+      label: 'GROSS MARGIN',
       dataName: 'grossMargin',
       additionalData: { decimals: 0 },
+      cols: ['label', 'trendCol', 'trendTotal', 'SO'], // No inven, only sales data
     },
     // {
     //   label: 'REVENUE $/LB',
     //   dataName: 'revenuePerLb',
     //   additionalData: { decimals: 2 },
+    //   cols: ['label', 'trendCol', 'trendTotal', 'SO'], // No inven, only sales data
     // },
     // {
     //   label: 'OTHP $/LB',
     //   dataName: 'othpPerLb',
     //   additionalData: { decimals: 2 },
+    //   cols: ['label', 'trendCol', 'trendTotal', 'SO'], // No inven, only sales data
     // },
     {
-      label: 'sales: NET REVENUE $/LB, inven: COST $/LB',
+      label: 'NET REVENUE/LB',
       dataName: 'netSalesPerLb',
       additionalData: { decimals: 2 },
+      cols: ['label', 'trendCol', 'trendTotal', 'SO'], // No inven, only sales data
     },
     {
-      label: 'COST $/LB',
+      label: 'COST/LB',
       dataName: 'cogsPerLb',
       additionalData: { decimals: 2 },
+      cols: ['label', 'trendCol', 'trendTotal', 'SO', 'InvenFG', 'InvenRM', 'PO'],
     },
     {
-      label: 'sales: GM $/LB, inven: COST $/LB',
+      label: 'GROSS MARGIN/LB',
       dataName: 'grossMarginPerLb',
       additionalData: { decimals: 2 },
+      cols: ['label', 'trendCol', 'trendTotal', 'SO'], // No inven, only sales data
     },
   ]
 }
