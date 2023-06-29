da8a320e49ca8da4accbaf4c9340fa0800094239
Eastern Dev <dev@seaflow.fish>
2023-06-29 15:59:30 -0400

    starting to test detail routes

--- reports/forProgBySpecBrndSize/routes/getDetail.js
+++ reports/forProgBySpecBrndSize/routes/getDetail.js
@@ -12,7 +12,6 @@ router.post('/', async (req, res) => {
   console.log(`\nget detail data for ${reportName} route HIT...`)
 
   // Note that start date is the END of the first week. Need the beginning of the same week to pull invoice dates that are after this:
-  const startWeek = await getStartOfWeek(periodStart)
 
   let response = null
 
