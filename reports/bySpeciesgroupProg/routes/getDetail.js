f64572f16d08344a73002895918c0d57a45c1b7f
Eastern Dev <dev@seaflow.fish>
2023-07-05 18:01:36 -0400

    working on adding detail reports to the species group-program report

--- reports/bySpeciesgroupProg/routes/getDetail.js
+++ reports/bySpeciesgroupProg/routes/getDetail.js
@@ -1,23 +1,53 @@
 const router = require('express').Router()
-const getDetail = require('../routes/getDetail')
+const getDetail_invenFg = require('../routines/getDetail_invenFg')
+const getDetail_salesOrder = require('../routines/getDetail_salesOrder')
+const getDetail_salesInvoice = require('../routines/getDetail_salesInvoice')
+const getDetail_purchaseOrder = require('../routines/getDetail_purchaseOrder')
 const { getStartOfWeek } = require('../../shared/queries/postgres/getDateStartByWeek')
 
-// @route   POST /api/sales/detail/bySpeciesgroupProg/
+// @route   POST /api/sales/detail/forProgBySpecBrndSize/
 // @desc    Get drilldown data for a given report and filter
 // @access  Private
 
 router.post('/', async (req, res) => {
-  const { program, option, filters, columnDataName, reportName, colType, periodStart, periodEnd } = req.body
+  const { program, option, filters, columnDataName, reportName, colType, periodStart, periodEnd, trend } = req.body
+
+  console.log('program: ', program)
+  console.log('option: ', option)
+  console.log('filters: ', filters)
+  console.log('columnDataName: ', columnDataName)
+  console.log('reportName: ', reportName)
+  console.log('colType: ', colType)
+  console.log('periodStart: ', periodStart)
+  console.log('periodEnd: ', periodEnd)
+  console.log('trend: ', trend)
 
   console.log(`\nget detail data for ${reportName} route HIT...`)
 
   // Note that start date is the END of the first week. Need the beginning of the same week to pull invoice dates that are after this:
-  const startWeek = await getStartOfWeek(periodStart)
+  let startWeek = await getStartOfWeek(periodStart)
+  startWeek = startWeek[0].formatted_date_start
 
-  const resp = await getDetail(program, option, filters, columnDataName, reportName, colType, startWeek, periodEnd)
+  let response = null
+
+  if (colType === 'invenFg') {
+    response = await getDetail_invenFg(program, filters, columnDataName)
+  }
+
+  if (colType === 'salesOrder') {
+    response = await getDetail_salesOrder(program, filters, columnDataName)
+  }
+
+  if (colType === 'salesInvoice') {
+    response = await getDetail_salesInvoice(program, filters, columnDataName, startWeek, periodEnd)
+  }
+
+  if (colType === 'purchaseOrder') {
+    response = await getDetail_purchaseOrder(program, filters)
+  }
 
   console.log(`get detail data for ${reportName} route COMPLETE. \n`)
-  res.send(resp)
+  res.send(response)
 })
 
 module.exports = router
