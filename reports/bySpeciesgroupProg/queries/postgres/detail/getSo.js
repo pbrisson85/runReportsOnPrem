655f6a4b5024558cb121a54809eb174eb1bdd447
Eastern Dev <dev@seaflow.fish>
2023-07-06 07:22:26 -0400

    working on adding detail reports to the species group-program report

--- reports/bySpeciesgroupProg/queries/postgres/detail/getSo.js
+++ reports/bySpeciesgroupProg/queries/postgres/detail/getSo.js
@@ -17,7 +17,7 @@ const lvl_1_subtotal_getSo_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num 
           
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND ms.fg_fresh_frozen = $3`, ['FG', program, filters[0]]
+      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL AND ms.species_group = $2`, ['FG', filters[0]]
      ) //prettier-ignore
 
     await pgClient.end()
@@ -44,7 +44,7 @@ const lvl_1_subtotal_getSoTagged_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num 
           
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND so.tagged_weight > 0 AND ms.fg_fresh_frozen = $3`, ['FG', program, filters[0]]
+      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL AND so.tagged_weight > 0 AND ms.species_group = $2`, ['FG', filters[0]]
    ) //prettier-ignore
 
     await pgClient.end()
@@ -71,7 +71,7 @@ const lvl_1_subtotal_getSoUntagged_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num 
           
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND so.untagged_weight > 0 AND ms.fg_fresh_frozen = $3`, ['FG', program, filters[0]]
+      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL AND so.untagged_weight > 0 AND ms.species_group = $2`, ['FG', filters[0]]
     ) //prettier-ignore
 
     await pgClient.end()
@@ -100,7 +100,7 @@ const lvl_2_subtotal_getSo_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num 
           
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND ms.fg_fresh_frozen = $3 AND ms.fg_treatment = $4`, ['FG', program, filters[0], filters[1]]
+      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL AND ms.species_group = $2 AND ms.program = $3`, ['FG', filters[0], filters[1]]
     ) //prettier-ignore
 
     await pgClient.end()
@@ -127,7 +127,7 @@ const lvl_2_subtotal_getSoTagged_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num 
           
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND so.tagged_weight > 0 AND ms.fg_fresh_frozen = $3 AND ms.fg_treatment = $4`, ['FG', program, filters[0], filters[1]]
+      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL AND so.tagged_weight > 0 AND ms.species_group = $2 AND ms.program = $3`, ['FG', filters[0], filters[1]]
     ) //prettier-ignore
 
     await pgClient.end()
@@ -154,90 +154,7 @@ const lvl_2_subtotal_getSoUntagged_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 
           
-      FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND so.untagged_weight > 0 AND ms.fg_fresh_frozen = $3 AND ms.fg_treatment = $4`, ['FG', program, filters[0], filters[1]]
-    ) //prettier-ignore
-
-    await pgClient.end()
-
-    return response.rows
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
-
-/* *********************************************** level 3 *********************************************** */
-
-const lvl_3_subtotal_getSo_detail = async (program, filters) => {
-  try {
-    const { Client } = require('pg')
-    const pgClient = new Client() // config from ENV
-    await pgClient.connect()
-
-    console.log(`level 3: query postgres for FG Sales Orders ...`)
-
-    const response = await pgClient.query(
-      `SELECT so.no_cost_found, so.customer_code, so.customer_name, so.so_num, so.location, so.so_line, so.formatted_ship_date, so.week_serial, so.item_num, ms.description, ms.species, ms.brand, ms.size_name, ms.fg_treatment, ms.fg_fresh_frozen, so.ext_weight AS lbs, so.ext_sales, so.ext_othp, so.ext_cost, so.gross_margin_ext, so.unit_price, so.rebate_lb, so.discount_lb, so.freight_lb, so.othp_lb, so.sales_net_lb, so.ave_cost_per_lb AS cost_lb, so.gross_margin_lb, so.tagged_weight, so.untagged_weight, so.ave_tagged_cost, so.ave_untagged_cost, so.ext_comm, so.commission_lb, so.sales_net_ext, so.ext_rebate, so.ext_discount, so.ext_freight, so.cost_ext_tagged, so.cost_ext_untagged
-      
-      FROM "salesReporting".sales_orders AS so
-        LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-          ON ms.item_num = so.item_num 
-          
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND ms.fg_fresh_frozen = $3 AND ms.fg_treatment = $4 AND ms.size_name = $5`, ['FG', program, filters[0], filters[1], filters[2]]
-     ) //prettier-ignore
-
-    await pgClient.end()
-
-    return response.rows
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
-
-const lvl_3_subtotal_getSoTagged_detail = async (program, filters) => {
-  try {
-    const { Client } = require('pg')
-    const pgClient = new Client() // config from ENV
-    await pgClient.connect()
-
-    console.log(`level 3: query postgres for FG Sales Orders ...`)
-
-    const response = await pgClient.query(
-      `SELECT so.no_cost_found, so.customer_code, so.customer_name, so.so_num, so.location, so.so_line, so.formatted_ship_date, so.week_serial, so.item_num, ms.description, ms.species, ms.brand, ms.size_name, ms.fg_treatment, ms.fg_fresh_frozen, so.tagged_weight AS lbs, COALESCE(so.tagged_weight * so.unit_price,0) AS ext_sales, COALESCE(so.tagged_weight * so.othp_lb,0) AS ext_othp, COALESCE(so.tagged_weight * ave_tagged_cost,0) AS ext_cost, COALESCE(so.tagged_weight * so.unit_price - so.tagged_weight * so.othp_lb - so.tagged_weight * ave_tagged_cost,0) AS gross_margin_ext, so.unit_price, so.rebate_lb, so.discount_lb, so.freight_lb, so.othp_lb, so.sales_net_lb, so.ave_tagged_cost AS cost_lb, COALESCE(so.sales_net_lb - so.ave_tagged_cost,0) AS gross_margin_lb, so.tagged_weight, so.untagged_weight, so.ave_tagged_cost, so.ave_untagged_cost, so.ext_comm, so.commission_lb, so.sales_net_ext, so.ext_rebate, so.ext_discount, so.ext_freight, so.cost_ext_tagged, so.cost_ext_untagged
-      
-      FROM "salesReporting".sales_orders AS so
-        LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-          ON ms.item_num = so.item_num 
-          
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND so.tagged_weight > 0 AND ms.fg_fresh_frozen = $3 AND ms.fg_treatment = $4 AND ms.size_name = $5`, ['FG', program,  filters[0], filters[1], filters[2]]
-    ) //prettier-ignore
-
-    await pgClient.end()
-
-    return response.rows
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
-
-const lvl_3_subtotal_getSoUntagged_detail = async (program, filters) => {
-  try {
-    const { Client } = require('pg')
-    const pgClient = new Client() // config from ENV
-    await pgClient.connect()
-
-    console.log(`level 3: query postgres for FG Sales Orders ...`)
-
-    const response = await pgClient.query(
-      `SELECT so.no_cost_found, so.customer_code, so.customer_name, so.so_num, so.location, so.so_line, so.formatted_ship_date, so.week_serial, so.item_num, ms.description, ms.species, ms.brand, ms.size_name, ms.fg_treatment, ms.fg_fresh_frozen, so.untagged_weight AS lbs, COALESCE(so.untagged_weight * so.unit_price,0) AS ext_sales, COALESCE(so.untagged_weight * so.othp_lb,0) AS ext_othp, COALESCE(so.untagged_weight * ave_untagged_cost,0) AS ext_cost, COALESCE(so.untagged_weight * so.unit_price - so.untagged_weight * so.othp_lb - so.untagged_weight * ave_untagged_cost,0) AS gross_margin_ext, so.unit_price, so.rebate_lb, so.discount_lb, so.freight_lb, so.othp_lb, so.sales_net_lb, so.ave_untagged_cost AS cost_lb, COALESCE(so.sales_net_lb - so.ave_untagged_cost,0) AS gross_margin_lb, so.tagged_weight, so.untagged_weight, so.ave_tagged_cost, so.ave_untagged_cost, so.ext_comm, so.commission_lb, so.sales_net_ext, so.ext_rebate, so.ext_discount, so.ext_freight, so.cost_ext_tagged, so.cost_ext_untagged
-      
-      FROM "salesReporting".sales_orders AS so
-        LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-          ON ms.item_num = so.item_num 
-          
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND so.untagged_weight > 0 AND ms.fg_fresh_frozen = $3 AND ms.fg_treatment = $4 AND ms.size_name = $5`, ['FG', program,  filters[0], filters[1], filters[2]]
+      FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL AND so.untagged_weight > 0 AND ms.species_group = $2 AND ms.program = $3`, ['FG', filters[0], filters[1]]
     ) //prettier-ignore
 
     await pgClient.end()
@@ -266,7 +183,7 @@ const lvl_0_total_getSo_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num 
           
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL`, ['FG', program]
+      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL`, ['FG']
     ) //prettier-ignore
 
     await pgClient.end()
@@ -293,7 +210,7 @@ const lvl_0_total_getSoTagged_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num 
       
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND so.tagged_weight > 0`, ['FG', program]
+      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL AND so.tagged_weight > 0`, ['FG']
     ) //prettier-ignore
 
     await pgClient.end()
@@ -320,7 +237,7 @@ const lvl_0_total_getSoUntagged_detail = async (program, filters) => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = so.item_num 
           
-      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.program = $2 AND ms.byproduct_type IS NULL AND so.untagged_weight > 0`, ['FG', program]
+      WHERE ms.item_type = $1 AND so.version = (SELECT MAX(version) - 1 FROM "salesReporting".sales_orders) AND ms.byproduct_type IS NULL AND so.untagged_weight > 0`, ['FG']
     ) //prettier-ignore
 
     await pgClient.end()
@@ -332,15 +249,12 @@ const lvl_0_total_getSoUntagged_detail = async (program, filters) => {
   }
 }
 
-module.exports.lvl_1_subtotal_getSo_detail = lvl_1_subtotal_getSo_detail
-module.exports.lvl_2_subtotal_getSo_detail = lvl_2_subtotal_getSo_detail
-module.exports.lvl_3_subtotal_getSo_detail = lvl_3_subtotal_getSo_detail
-module.exports.lvl_3_subtotal_getSoTagged_detail = lvl_3_subtotal_getSoTagged_detail
-module.exports.lvl_3_subtotal_getSoUntagged_detail = lvl_3_subtotal_getSoUntagged_detail
 module.exports.lvl_0_total_getSo_detail = lvl_0_total_getSo_detail
 module.exports.lvl_0_total_getSoTagged_detail = lvl_0_total_getSoTagged_detail
 module.exports.lvl_0_total_getSoUntagged_detail = lvl_0_total_getSoUntagged_detail
+module.exports.lvl_2_subtotal_getSo_detail = lvl_2_subtotal_getSo_detail
 module.exports.lvl_2_subtotal_getSoTagged_detail = lvl_2_subtotal_getSoTagged_detail
 module.exports.lvl_2_subtotal_getSoUntagged_detail = lvl_2_subtotal_getSoUntagged_detail
+module.exports.lvl_1_subtotal_getSo_detail = lvl_1_subtotal_getSo_detail
 module.exports.lvl_1_subtotal_getSoTagged_detail = lvl_1_subtotal_getSoTagged_detail
 module.exports.lvl_1_subtotal_getSoUntagged_detail = lvl_1_subtotal_getSoUntagged_detail
