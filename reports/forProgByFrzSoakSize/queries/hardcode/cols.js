7086965cc34e2b652d071ba61785ee63caf900dd
Eastern Dev <dev@seaflow.fish>
2023-07-07 14:32:45 -0400

    small tweaks

--- reports/forProgByFrzSoakSize/queries/hardcode/cols.js
+++ reports/forProgByFrzSoakSize/queries/hardcode/cols.js
@@ -7,7 +7,7 @@ const cols = [
     filterName: 'l1_filter', // key to match up the column with the filter
     justifyData: 'start', // css justify content
     justifyHeading: 'center', // css justify content
-    width: '200px', // css width
+    width: '150px', // css width
     number: false, // flag to use formatTableDataNumber model
     decimals: 0, // if number is true, decimals will be used
     leftSticky: true, // css sticky
@@ -27,11 +27,11 @@ const cols = [
     filterName: 'l2_filter', // key to match up the column with the filter
     justifyData: 'start', // css justify content
     justifyHeading: 'center', // css justify content
-    width: '175px', // css width
+    width: '150px', // css width
     number: false, // flag to use formatTableDataNumber model
     decimals: 0, // if number is true, decimals will be used
     leftSticky: true, // css sticky
-    left: '200px', // css positioning for sticky sum of prior col widths
+    left: '150px', // css positioning for sticky sum of prior col widths
     hidden: false, // flag to hide column.
     view: null, // dataset to show for the column (extended_cost, weight, cost_per_lb)
     data: false,
@@ -51,7 +51,7 @@ const cols = [
     number: false, // flag to use formatTableDataNumber model
     decimals: 0, // if number is true, decimals will be used
     leftSticky: true, // css sticky
-    left: '375px', // css positioning for sticky sum of prior col widths
+    left: '300px', // css positioning for sticky sum of prior col widths
     hidden: false, // flag to hide column.
     view: null, // dataset to show for the column (extended_cost, weight, cost_per_lb)
     data: false,
