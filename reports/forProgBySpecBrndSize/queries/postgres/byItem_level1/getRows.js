c263bd136ca9cfc64e81b08b5d042066d050de98
Eastern Dev <dev@seaflow.fish>
2023-07-11 09:43:01 -0400

    adding sales trend by fiscal year

--- reports/forProgBySpecBrndSize/queries/postgres/byItem_level1/getRows.js
+++ reports/forProgBySpecBrndSize/queries/postgres/byItem_level1/getRows.js
@@ -21,7 +21,7 @@ const getRowsFirstLevelDetail = async (start, end, program, filters) => {
           LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = sales_line_items.item_number 
           
-            WHERE sales_line_items.formatted_invoice_date >= $1 AND sales_line_items.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 
+            WHERE ms.byproduct_type IS NULL AND ms.item_type = $1 AND ms.program = $2 AND ms.species = $3 
           
           GROUP BY ms.item_num, ms.description, ms.brand, ms.size_name 
           
@@ -30,7 +30,7 @@ const getRowsFirstLevelDetail = async (start, end, program, filters) => {
           LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = perpetual_inventory.item_number 
           
-            WHERE ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND ms.species = $5 
+            WHERE ms.byproduct_type IS NULL AND ms.item_type = $1 AND ms.program = $2 AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) AND ms.species = $3 
           
           GROUP BY ms.item_num, ms.description, ms.brand, ms.size_name 
           
@@ -39,10 +39,10 @@ const getRowsFirstLevelDetail = async (start, end, program, filters) => {
           LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = sales_orders.item_num 
           
-            WHERE ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND sales_orders.version = (SELECT MAX(sales_orders.version) - 1 FROM "salesReporting".sales_orders) AND ms.species = $5 
+            WHERE ms.byproduct_type IS NULL AND ms.item_type = $1 AND ms.program = $2 AND sales_orders.version = (SELECT MAX(sales_orders.version) - 1 FROM "salesReporting".sales_orders) AND ms.species = $3 
           
           GROUP BY ms.item_num, ms.description, ms.brand, ms.size_name`,
-        [start, end, 'FG', program, filters[0]]
+        [ 'FG', program, filters[0]]
         ) //prettier-ignore
 
     await pgClient.end()
