2da9c247c5a0770c80b48fb7ab848f583a81acff
Eastern Dev <dev@seaflow.fish>
2023-07-25 18:46:44 -0400

    adding get item drilldown from customer drilldown

--- reports/forProgByFrzBrndSize/routes/buildDrillDown_getItemsByCust.js
+++ reports/forProgByFrzBrndSize/routes/buildDrillDown_getItemsByCust.js
@@ -34,21 +34,25 @@ router.post('/', async (req, res) => {
   let response = null
 
   if (filters[1] === 'SUBTOTAL') {
+    console.log('hit level 1')
     // level 1 subtotal
     response = await buildDrillDown_byItem_level1(program, startWeek[0].formatted_date_start, periodEnd, filters, showFyTrend)
   }
 
   if (filters[1] !== 'SUBTOTAL' && filters[2] === 'SUBTOTAL') {
+    console.log('hit level 2')
     // level 2 subtotal
     response = await buildDrillDown_byItem_level2(program, startWeek[0].formatted_date_start, periodEnd, filters, showFyTrend)
   }
 
   if (filters[1] !== 'TOTAL' && filters[1] !== 'SUBTOTAL' && filters[2] !== 'SUBTOTAL') {
+    console.log('hit level 3')
     // level 3 subtotal
     response = await buildDrillDown_byItem_level3(program, startWeek[0].formatted_date_start, periodEnd, filters, showFyTrend)
   }
 
   if (filters[1] === 'TOTAL') {
+    console.log('hit level 0')
     // level 0 total
     response = await buildDrillDown_byItem_level0(program, startWeek[0].formatted_date_start, periodEnd, filters, showFyTrend)
   }
