01abede84dac3a1def10960c6cd043e2d4196cc1
Eastern Dev <dev@seaflow.fish>
2023-07-25 17:50:39 -0400

    updated view filters

--- reports/filters/queries/hardcode/getViewFilters.js
+++ reports/filters/queries/hardcode/getViewFilters.js
@@ -4,67 +4,67 @@ const getViewFilters = () => {
       label: 'WEIGHT (lbs)',
       dataName: 'weight',
       additionalData: { decimals: 0 },
-      cols: ['label', 'salesInvoice', 'salesOrder', 'invenFg', 'invenRm', 'purchaseOrder'], // list of colTypes that should be displayed in this view
+      cols: ['label', 'salesInvoice', 'salesOrder', 'invenFg', 'invenRm', 'purchaseOrder', 'salesKpi'], // list of colTypes that should be displayed in this view
     },
     // {
     //   label: 'REVENUE $',
     //   dataName: 'revenue',
     //   additionalData: { decimals: 0 },
-    //   cols: ['label',  'salesInvoice', 'salesOrder'], // No inven, only sales data
+    //   cols: ['label',  'salesInvoice', 'salesOrder', 'salesKpi'], // No inven, only sales data
     // },
     // {
     //   label: 'OTHP $',
     //   dataName: 'othp',
     //   additionalData: { decimals: 0 },
-    //   cols: ['label',  'salesInvoice', 'salesOrder'], // No inven, only sales data
+    //   cols: ['label',  'salesInvoice', 'salesOrder', 'salesKpi'], // No inven, only sales data
     // },
     // {
     //   label: 'NET REVENUE',
     //   dataName: 'netSales',
     //   additionalData: { decimals: 0 },
-    //   cols: ['label', 'salesInvoice', 'salesOrder'], // No inven, only sales data
+    //   cols: ['label', 'salesInvoice', 'salesOrder', 'salesKpi'], // No inven, only sales data
     // },
     {
       label: 'COST',
       dataName: 'cogs',
       additionalData: { decimals: 0 },
-      cols: ['label', 'salesInvoice', 'salesOrder', 'invenFg', 'invenRm', 'purchaseOrder'],
+      cols: ['label', 'salesInvoice', 'salesOrder', 'invenFg', 'invenRm', 'purchaseOrder', 'salesKpi'],
     },
     {
       label: 'GROSS MARGIN',
       dataName: 'grossMargin',
       additionalData: { decimals: 0 },
-      cols: ['label', 'salesInvoice', 'salesOrder'], // No inven, only sales data
+      cols: ['label', 'salesInvoice', 'salesOrder', 'salesKpi'], // No inven, only sales data
     },
     // {
     //   label: 'REVENUE $/LB',
     //   dataName: 'revenuePerLb',
     //   additionalData: { decimals: 2 },
-    //   cols: ['label',  'salesInvoice', 'salesOrder'], // No inven, only sales data
+    //   cols: ['label',  'salesInvoice', 'salesOrder', 'salesKpi'], // No inven, only sales data
     // },
     // {
     //   label: 'OTHP $/LB',
     //   dataName: 'othpPerLb',
     //   additionalData: { decimals: 2 },
-    //   cols: ['label',  'salesInvoice', 'salesOrder'], // No inven, only sales data
+    //   cols: ['label',  'salesInvoice', 'salesOrder', 'salesKpi'], // No inven, only sales data
     // },
     {
       label: 'NET REVENUE/LB',
       dataName: 'netSalesPerLb',
       additionalData: { decimals: 2 },
-      cols: ['label', 'salesInvoice', 'salesOrder'], // No inven, only sales data
+      cols: ['label', 'salesInvoice', 'salesOrder', 'salesKpi'], // No inven, only sales data
     },
     {
       label: 'COST/LB',
       dataName: 'cogsPerLb',
       additionalData: { decimals: 2 },
-      cols: ['label', 'salesInvoice', 'salesOrder', 'invenFg', 'invenRm', 'purchaseOrder'],
+      cols: ['label', 'salesInvoice', 'salesOrder', 'invenFg', 'invenRm', 'purchaseOrder', 'salesKpi'],
     },
     {
       label: 'GROSS MARGIN/LB',
       dataName: 'grossMarginPerLb',
       additionalData: { decimals: 2 },
-      cols: ['label', 'salesInvoice', 'salesOrder'], // No inven, only sales data
+      cols: ['label', 'salesInvoice', 'salesOrder', 'salesKpi'], // No inven, only sales data
     },
   ]
 }
