dcd047aa913801ae94c8a9f638049866eea51907
Eastern Dev <dev@seaflow.fish>
2023-08-01 12:42:14 -0400

    added a flag to fy trend cols for frint end styling

--- reports/shared/queries/postgres/getFiscalYearCols.js
+++ reports/shared/queries/postgres/getFiscalYearCols.js
@@ -8,11 +8,13 @@ const getFiscalYearCols = async () => {
   const fys = await pgClient.query(
     `SELECT 
       DISTINCT(sl.fiscal_year) AS dataName, 
-      sl.fiscal_year AS displayName, TRUE AS showFyTrendflag
+      sl.fiscal_year AS displayName, TRUE AS fyTrendCol
     FROM "salesReporting".sales_line_items AS sl 
     
     ORDER BY sl.fiscal_year ASC`
   )
+
+  // note that fyTrendCol is used for styling on front end
 
   await pgClient.end()
 
@@ -29,11 +31,13 @@ const getFiscalYearYtdCols = async () => {
   const fys = await pgClient.query(
     `SELECT 
       DISTINCT(sl.fiscal_year) || '_ytd' AS dataName, 
-      sl.fiscal_year  || ' YTD' AS displayName, TRUE AS showFyTrendflag
+      sl.fiscal_year  || ' YTD' AS displayName, TRUE AS fyYtdTrendCol
     FROM "salesReporting".sales_line_items AS sl 
     
     ORDER BY sl.fiscal_year || '_ytd' ASC`
   )
+
+  // note that fyYtdTrendCol is used for styling on front end
 
   await pgClient.end()
 
