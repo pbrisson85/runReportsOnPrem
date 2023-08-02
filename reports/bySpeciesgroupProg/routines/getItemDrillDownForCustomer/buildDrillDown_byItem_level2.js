e54f76389573553604515ccbcf833db8221fcdcd
Eastern Dev <dev@seaflow.fish>
2023-08-02 15:10:25 -0400

    adding ave weekly sales and weeks inven on hand

--- reports/bySpeciesgroupProg/routines/getItemDrillDownForCustomer/buildDrillDown_byItem_level2.js
+++ reports/bySpeciesgroupProg/routines/getItemDrillDownForCustomer/buildDrillDown_byItem_level2.js
@@ -58,6 +58,8 @@ const combineMappedRows = require('../../../shared/models/combineMappedRows')
 const cleanLabelsForDisplay = require('../../../shared/models/cleanLabelsForDisplay')
 const unflattenByCompositKey = require('../../../shared/models/unflattenByCompositKey')
 const calcPercentSalesCol = require('../../../shared/models/calcPercentSalesCol')
+const calcAveWeeklySales = require('../../../shared/models/calcAveWeeklySales')
+const calcWeeksInvOnHand = require('../../../shared/models/calcWeeksInvOnHand')
 const labelCols = require('../../queries/hardcode/cols_byItem_level2')
 
 const buildDrillDown = async (program, start, end, filters, showFyTrend, startWeek, endWeek) => {
@@ -126,6 +128,15 @@ const buildDrillDown = async (program, start, end, filters, showFyTrend, startWe
   const lvl_1_percent_reportTotal = calcPercentSalesCol(lvl_0_total_salesPeriodToDate[0], lvl_1_subtotal_salesPeriodToDate, 'percentReportTotal')
   const lvl_0_percent_reportTotal = calcPercentSalesCol(lvl_0_total_salesPeriodToDate[0], lvl_0_total_salesPeriodToDate, 'percentReportTotal')
 
+  /* AVE WEEKLY SALES */
+  const weeks = endWeek - startWeek + 1
+  const lvl_1_aveWeeklySales = calcAveWeeklySales(lvl_1_subtotal_salesPeriodToDate, 'aveWeeklySales', weeks)
+  const lvl_0_aveWeeklySales = calcAveWeeklySales(lvl_0_total_salesPeriodToDate, 'aveWeeklySales', weeks)
+
+  /* WEEKS INV ON HAND */
+  const lvl_1_weeksInvOnHand = calcWeeksInvOnHand(lvl_1_subtotal_fgInven, lvl_1_aveWeeklySales, 'weeksInvenOnHand', 1)
+  const lvl_0_weeksInvOnHand = calcWeeksInvOnHand(lvl_0_total_fgInven, lvl_0_aveWeeklySales, 'weeksInvenOnHand', 1)
+
   ///////////////////////////////// ROWS
   let rowsFirstLevelDetail
   if (showFyTrend) {
@@ -174,6 +185,8 @@ const buildDrillDown = async (program, start, end, filters, showFyTrend, startWe
       ...lvl_0_percent_companySales,
       ...lvl_1_percent_reportTotal,
       ...lvl_0_percent_reportTotal,
+      ...lvl_1_aveWeeklySales,
+      ...lvl_0_aveWeeklySales,
     ],
     rowTemplate_unflat
   )
@@ -192,6 +205,8 @@ const buildDrillDown = async (program, start, end, filters, showFyTrend, startWe
       ...lvl_0_total_fgAtLoc_tagged,
       ...lvl_1_subtotal_fgPo,
       ...lvl_0_total_fgPo,
+      ...lvl_1_weeksInvOnHand,
+      ...lvl_0_weeksInvOnHand,
     ],
     rowTemplate_unflat
   )
