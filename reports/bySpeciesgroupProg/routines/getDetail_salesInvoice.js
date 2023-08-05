92807d82fecc7db7ddd2e7a6ca9286040baff1f7
Eastern Dev <dev@seaflow.fish>
2023-08-05 18:31:54 -0400

    updated get detail on all reports to include prior year trends

--- reports/bySpeciesgroupProg/routines/getDetail_salesInvoice.js
+++ reports/bySpeciesgroupProg/routines/getDetail_salesInvoice.js
@@ -9,17 +9,17 @@ const getDetail = async (program, filters, startWeek, endWeek, year) => {
 
   if (filters[0] === 'SUBTOTAL' || filters[1] === 'SUBTOTAL') {
     // get level 1 subtotal where freeze = filters[0] and soak = filters[1] and size = filters[2]
-    detail = await lvl_1_subtotal_getSales_detail(startWeek, endWeek, program, filters, year)
+    detail = await lvl_1_subtotal_getSales_detail(startWeek, endWeek, program, filters, year, year)
   }
 
   if (filters[0] !== 'SUBTOTAL' && filters[1] !== 'SUBTOTAL') {
     // get level 2 subtotal where freeze = filters[0] and soak = filters[1] and size = filters[2]
-    detail = await lvl_2_subtotal_getSales_detail(startWeek, endWeek, program, filters, year)
+    detail = await lvl_2_subtotal_getSales_detail(startWeek, endWeek, program, filters, year, year)
   }
 
   if (filters[1] === 'TOTAL') {
     // get level 0 total where freeze = filters[0] and soak = filters[1] and size = filters[2]
-    detail = await lvl_0_total_getSales_detail(startWeek, endWeek, program, filters, year)
+    detail = await lvl_0_total_getSales_detail(startWeek, endWeek, program, filters, year, year)
   }
 
   return detail
--- reports/forProgBySpecSoakSize/queries/postgres/detail/getSales.js
+++ reports/forProgBySpecSoakSize/queries/postgres/detail/getSales.js
@@ -1,40 +1,8 @@
 /* *********************************************** Level 1 Group *********************************************** */
 
-// FG Species Group totals by week
-
-const lvl_1_subtotal_getSalesByWk_detail = async (program, filters, columnDataName, start, end) => {
-  try {
-    const { Client } = require('pg')
-    const pgClient = new Client() // config from ENV
-    await pgClient.connect()
-
-    console.log(`level 1: query postgres to get FG sales data by week ...`)
-
-    // STILL NEED so.rebate_lb, so.discount_lb, so.freight_lb, so.ext_comm, so.commission_lb, so.ext_rebate, so.ext_discount, so.ext_freight ************
-
-    const response = await pgClient.query(
-      `SELECT sl.net_sales_ext, sl.gross_margin_lb, sl.cost_lb, sl.net_sales_lb, sl.othp_lb, sl.gross_sales_lb, sl.location, sl.customer_code, sl.customer_name, sl.invoice_number, sl.line_number, sl.formatted_invoice_date, sl.week_serial, sl.item_number, ms.description, ms.species, ms.brand, ms.size_name, ms.fg_treatment, ms.fg_fresh_frozen, sl.calc_gm_rept_weight, sl.gross_sales_ext, sl.othp_ext, sl.cogs_ext_gl, sl.gross_margin_ext
-      
-      FROM "salesReporting".sales_line_items AS sl
-        LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-          ON ms.item_num = sl.item_number 
-          
-      WHERE sl.formatted_invoice_date >= $1 AND sl.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 AND sl.week_serial = $6`,
-      [start, end, 'FG', program, filters[0], columnDataName]
-      ) //prettier-ignore
-
-    await pgClient.end()
-
-    return response.rows
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
-
 // FG Species Group col total for period
 
-const lvl_1_subtotal_getSalesPeriodToDate_detail = async (start, end, program, filters) => {
+const lvl_1_subtotal_getSalesPeriodToDate_detail = async (start, end, program, filters, year) => {
   try {
     const { Client } = require('pg')
     const pgClient = new Client() // config from ENV
@@ -49,8 +17,8 @@ const lvl_1_subtotal_getSalesPeriodToDate_detail = async (start, end, program, f
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = sl.item_number 
           
-      WHERE sl.formatted_invoice_date >= $1 AND sl.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5`,
-      [start, end, 'FG', program, filters[0]]
+      WHERE sl.week >= $1 AND sl.week <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 AND sl.fiscal_year = $6`,
+      [start, end, 'FG', program, filters[0], year]
       ) //prettier-ignore
 
     await pgClient.end()
@@ -64,39 +32,9 @@ const lvl_1_subtotal_getSalesPeriodToDate_detail = async (start, end, program, f
 
 /* *********************************************** Level 2 Group *********************************************** */
 
-// FG Program row totals by week
-
-const lvl_2_subtotal_getSalesByWk_detail = async (program, filters, columnDataName, start, end) => {
-  try {
-    const { Client } = require('pg')
-    const pgClient = new Client() // config from ENV
-    await pgClient.connect()
-
-    console.log(`level 2: query postgres to get FG sales data by week ...`)
-
-    const response = await pgClient.query(
-      `SELECT sl.net_sales_ext, sl.gross_margin_lb, sl.cost_lb, sl.net_sales_lb, sl.othp_lb, sl.gross_sales_lb, sl.location, sl.customer_code, sl.customer_name, sl.invoice_number, sl.line_number, sl.formatted_invoice_date, sl.week_serial, sl.item_number, ms.description, ms.species, ms.brand, ms.size_name, ms.fg_treatment, ms.fg_fresh_frozen, sl.calc_gm_rept_weight, sl.gross_sales_ext, sl.othp_ext, sl.cogs_ext_gl, sl.gross_margin_ext 
-      
-      FROM "salesReporting".sales_line_items AS sl 
-        LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-          ON ms.item_num = sl.item_number 
-          
-      WHERE sl.formatted_invoice_date >= $1 AND sl.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 AND ms.fg_treatment = $6 AND sl.week_serial = $7`,
-      [start, end, 'FG', program, filters[0], filters[1], columnDataName]
-      ) //prettier-ignore
-
-    await pgClient.end()
-
-    return response.rows
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
-
 // FG Program col total for period
 
-const lvl_2_subtotal_getSalesPeriodToDate_detail = async (start, end, program, filters) => {
+const lvl_2_subtotal_getSalesPeriodToDate_detail = async (start, end, program, filters, year) => {
   try {
     const { Client } = require('pg')
     const pgClient = new Client() // config from ENV
@@ -111,8 +49,8 @@ const lvl_2_subtotal_getSalesPeriodToDate_detail = async (start, end, program, f
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = sl.item_number 
           
-      WHERE sl.formatted_invoice_date >= $1 AND sl.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 AND ms.fg_treatment = $6`,
-      [start, end, 'FG', program, filters[0], filters[1]]
+      WHERE sl.week >= $1 AND sl.week <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 AND ms.fg_treatment = $6 AND sl.fiscal_year = $7`,
+      [start, end, 'FG', program, filters[0], filters[1], year]
       ) //prettier-ignore
 
     await pgClient.end()
@@ -126,39 +64,9 @@ const lvl_2_subtotal_getSalesPeriodToDate_detail = async (start, end, program, f
 
 /* *********************************************** Level 3 Group *********************************************** */
 
-// FG Program row totals by week
-
-const lvl_3_subtotal_getSalesByWk_detail = async (program, filters, columnDataName, start, end) => {
-  try {
-    const { Client } = require('pg')
-    const pgClient = new Client() // config from ENV
-    await pgClient.connect()
-
-    console.log(`level 3: query postgres to get FG sales data by week ...`)
-
-    const response = await pgClient.query(
-      `SELECT sl.net_sales_ext, sl.gross_margin_lb, sl.cost_lb, sl.net_sales_lb, sl.othp_lb, sl.gross_sales_lb, sl.location, sl.customer_code, sl.customer_name, sl.invoice_number, sl.line_number, sl.formatted_invoice_date, sl.week_serial, sl.item_number, ms.description, ms.species, ms.brand, ms.size_name, ms.fg_treatment, ms.fg_fresh_frozen, sl.calc_gm_rept_weight, sl.gross_sales_ext, sl.othp_ext, sl.cogs_ext_gl, sl.gross_margin_ext 
-      
-      FROM "salesReporting".sales_line_items AS sl 
-        LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-          ON ms.item_num = sl.item_number 
-          
-      WHERE sl.formatted_invoice_date >= $1 AND sl.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 AND ms.fg_treatment = $6 AND ms.size_name = $7 AND sl.week_serial = $8`,
-      [start, end, 'FG', program, filters[0], filters[1], filters[2], columnDataName]
-      ) //prettier-ignore
-
-    await pgClient.end()
-
-    return response.rows
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
-
 // FG Program col total for period
 
-const lvl_3_subtotal_getSalesPeriodToDate_detail = async (start, end, program, filters) => {
+const lvl_3_subtotal_getSalesPeriodToDate_detail = async (start, end, program, filters, year) => {
   try {
     const { Client } = require('pg')
     const pgClient = new Client() // config from ENV
@@ -173,8 +81,8 @@ const lvl_3_subtotal_getSalesPeriodToDate_detail = async (start, end, program, f
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = sl.item_number 
           
-      WHERE sl.formatted_invoice_date >= $1 AND sl.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 AND ms.fg_treatment = $6 AND ms.size_name = $7`,
-      [start, end, 'FG', program, filters[0], filters[1], filters[2]]
+      WHERE sl.week >= $1 AND sl.week <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND ms.species = $5 AND ms.fg_treatment = $6 AND ms.size_name = $7 AND sl.fiscal_year = $8`,
+      [start, end, 'FG', program, filters[0], filters[1], filters[2], year]
       ) //prettier-ignore
 
     await pgClient.end()
@@ -188,39 +96,9 @@ const lvl_3_subtotal_getSalesPeriodToDate_detail = async (start, end, program, f
 
 /* *********************************************** Totals *********************************************** */
 
-// All sales row totals by week for a program
-
-const lvl_0_total_getSalesByWk_detail = async (program, filters, columnDataName, start, end) => {
-  try {
-    const { Client } = require('pg')
-    const pgClient = new Client() // config from ENV
-    await pgClient.connect()
-
-    console.log(`level 0: query postgres to get FG sales data by week ...`)
-
-    const response = await pgClient.query(
-      `SELECT sl.net_sales_ext, sl.gross_margin_lb, sl.cost_lb, sl.net_sales_lb, sl.othp_lb, sl.gross_sales_lb, sl.location, sl.customer_code, sl.customer_name, sl.invoice_number, sl.line_number, sl.formatted_invoice_date, sl.week_serial, sl.item_number, ms.description, ms.species, ms.brand, ms.size_name, ms.fg_treatment, ms.fg_fresh_frozen, sl.calc_gm_rept_weight, sl.gross_sales_ext, sl.othp_ext, sl.cogs_ext_gl, sl.gross_margin_ext
-      
-      FROM "salesReporting".sales_line_items AS sl 
-        LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-          ON ms.item_num = sl.item_number 
-          
-      WHERE sl.formatted_invoice_date >= $1 AND sl.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND sl.week_serial = $5`,
-      [start, end, 'FG', program, columnDataName]
-    ) //prettier-ignore
-
-    await pgClient.end()
-
-    return response.rows
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
-
 // All sales col total for a program
 
-const lvl_0_total_getSalesPeriodToDate_detail = async (start, end, program, filters) => {
+const lvl_0_total_getSalesPeriodToDate_detail = async (start, end, program, filters, year) => {
   try {
     const { Client } = require('pg')
     const pgClient = new Client() // config from ENV
@@ -235,8 +113,8 @@ const lvl_0_total_getSalesPeriodToDate_detail = async (start, end, program, filt
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = sl.item_number 
           
-      WHERE sl.formatted_invoice_date >= $1 AND sl.formatted_invoice_date <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4`,
-      [start, end, 'FG', program]
+      WHERE sl.week >= $1 AND sl.week <= $2 AND ms.byproduct_type IS NULL AND ms.item_type = $3 AND ms.program = $4 AND sl.fiscal_year = $5`,
+      [start, end, 'FG', program, year]
     ) //prettier-ignore
 
     await pgClient.end()
@@ -248,11 +126,7 @@ const lvl_0_total_getSalesPeriodToDate_detail = async (start, end, program, filt
   }
 }
 
-module.exports.lvl_0_total_getSalesByWk_detail = lvl_0_total_getSalesByWk_detail
 module.exports.lvl_0_total_getSalesPeriodToDate_detail = lvl_0_total_getSalesPeriodToDate_detail
-module.exports.lvl_2_subtotal_getSalesByWk_detail = lvl_2_subtotal_getSalesByWk_detail
 module.exports.lvl_2_subtotal_getSalesPeriodToDate_detail = lvl_2_subtotal_getSalesPeriodToDate_detail
-module.exports.lvl_1_subtotal_getSalesByWk_detail = lvl_1_subtotal_getSalesByWk_detail
 module.exports.lvl_1_subtotal_getSalesPeriodToDate_detail = lvl_1_subtotal_getSalesPeriodToDate_detail
-module.exports.lvl_3_subtotal_getSalesByWk_detail = lvl_3_subtotal_getSalesByWk_detail
 module.exports.lvl_3_subtotal_getSalesPeriodToDate_detail = lvl_3_subtotal_getSalesPeriodToDate_detail
