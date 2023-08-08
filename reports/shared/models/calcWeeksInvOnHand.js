a5b23f0fbb86b47483012c0b8bc6fd7522e30e2a
Eastern Dev <dev@seaflow.fish>
2023-08-08 17:47:00 -0400

    simplified calcWeeksInvOnHand

--- reports/shared/models/calcWeeksInvOnHand.js
+++ reports/shared/models/calcWeeksInvOnHand.js
@@ -1,50 +1,27 @@
-const requestEmailNotification = require('../../../requests/requestEmail')
-
 const unflattenByCompositKey = require('./unflattenByCompositKey')
 
-const calcWeeksInvOnHand = (invenData, salesData, colName, numLabels) => {
+const calcWeeksInvOnHand = (invenData, salesData, colName) => {
   // Do not know how many columns the data is grouped by
-  let sales_unflat = {}
-  if (numLabels === 1) {
-    sales_unflat = unflattenByCompositKey(salesData, { 1: 'l1_label' })
-  } else if (numLabels === 2) {
-    sales_unflat = unflattenByCompositKey(salesData, { 1: 'l1_label', 2: 'l2_label' })
-  } else if (numLabels === 3) {
-    sales_unflat = unflattenByCompositKey(salesData, { 1: 'l1_label', 2: 'l2_label', 3: 'l3_label' })
-  } else {
-    requestEmailNotification(`Error in calcWeeksInvOnHand.js: numLabels is not 1, 2, or 3. numLabels = ${numLabels}`)
-  }
+  const sales_unflat = unflattenByCompositKey(salesData, {
+    1: 'l1_label',
+    2: 'l2_label',
+    3: 'l3_label',
+    4: 'l4_label',
+    5: 'l5_label',
+    6: 'l6_label',
+  })
 
   const weeksInvOnHand = invenData.map(row => {
-    const { lbs, cogs, l1_label, l2_label, l3_label } = row
+    const { lbs, cogs, l1_label, l2_label, l3_label, l4_label, l5_label, l6_label } = row
 
     let lbsPerWeek = null
     let cogsPerWeek = null
     let key = null
     let calcData = null
-    if (numLabels === 1) {
-      key = `${l1_label}`
-      lbsPerWeek = sales_unflat[key]?.lbs ?? 0
-      cogsPerWeek = sales_unflat[key]?.cogs ?? 0
-    } else if (numLabels === 2) {
-      key = `${l1_label}-${l2_label}`
-      lbsPerWeek = sales_unflat[key]?.lbs ?? 0
-      cogsPerWeek = sales_unflat[key]?.cogs ?? 0
-    } else if (numLabels === 3) {
-      key = `${l1_label}-${l2_label}-${l3_label}`
-      lbsPerWeek = sales_unflat[key]?.lbs ?? 0
-      cogsPerWeek = sales_unflat[key]?.cogs ?? 0
-    } else {
-      calcData = {
-        ...row,
-        column: colName,
-        lbs: 0,
-        cogs: 0,
-        weeksOnHandFlag: true, // in inventory rows map will not calc / lb, will only use the weight or cogs ratio
-      }
 
-      return calcData
-    }
+    key = `${l1_label}-${l2_label}-${l3_label}-${l4_label}-${l5_label}-${l6_label}`
+    lbsPerWeek = sales_unflat[key]?.lbs ?? 0
+    cogsPerWeek = sales_unflat[key]?.cogs ?? 0
 
     calcData = {
       ...row,
