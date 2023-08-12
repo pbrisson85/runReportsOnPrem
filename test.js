f70961d632c17d22be289d133e9be6c7aca39974
Eastern Dev <dev@seaflow.fish>
2023-08-12 12:22:31 -0400

    testing postgres js

--- test.js
+++ test.js
@@ -6,16 +6,17 @@ const postgres = require('postgres')
 const sql = postgres()
 
 router.get('/', async (req, res) => {
-  const customerFilter = 'DAMI00'
   const fileds = ['ms.item_num', 'ms.species']
   const table = `invenReporting.master_supplement`
   const fromAlias = 'ms'
+  const whereField = 'ms.private_label_cust'
+  const customerFilter = 'DAMI00'
 
   const item = await sql`
     select
       ${sql(fileds)}
     from ${sql(table)} AS ${sql(fromAlias)}
-    where ms.private_label_cust = ${customerFilter}
+    where ${sql(whereField)} = ${customerFilter}
   `
 
   res.send(item)
