97275ede477e5b9d906ba3a8eb05399936cfbfe3
Eastern Dev <dev@seaflow.fish>
2023-08-13 17:03:48 -0400

    converting to postgres js from pg - consolidating all three level get detail routes

--- reports/forProgByFrzSoakSize/routes/getDetail.js
+++ reports/forProgByFrzSoakSize/routes/getDetail.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
-const getDetail_invenFg = require('../routines/getDetail_invenFg')
-const getDetail_salesOrder = require('../routines/getDetail_salesOrder')
-const getDetail_salesInvoice = require('../routines/getDetail_salesInvoice')
-const getDetail_purchaseOrder = require('../routines/getDetail_purchaseOrder')
+const getDetail_invenFg = require('../../shared/routines/getDetail_invenFg')
+const getDetail_salesOrder = require('../../shared/routines/getDetail_salesOrder')
+const getDetail_salesInvoice = require('../../shared/routines/getDetail_salesInvoice')
+const getDetail_purchaseOrder = require('../../shared/routines/getDetail_purchaseOrder')
 const { getWeekForDate } = require('../../shared/queries/postgres/getWeekForDate')
 
 // @route   POST /api/sales/detail/forProgBySpecBrndSize/
@@ -13,16 +13,22 @@ router.post('/', async (req, res) => {
   const { program, option, filters, columnDataName, reportName, colType, periodStart, periodEnd, fyTrendCol, fyYtdTrendCol } = req.body
   let { year } = req.body
 
+  const config = {
+    l1_field: 'ms.fg_fresh_frozen',
+    l2_field: 'ms.fg_treatment',
+    l3_field: 'ms.size_name',
+  }
+
   console.log(`\nget detail data for ${reportName} route HIT...`)
 
   let response = null
 
   if (colType === 'invenFg') {
-    response = await getDetail_invenFg(program, filters, columnDataName)
+    response = await getDetail_invenFg(config, program, filters, columnDataName)
   }
 
   if (colType === 'salesOrder') {
-    response = await getDetail_salesOrder(program, filters, columnDataName)
+    response = await getDetail_salesOrder(config, program, filters, columnDataName)
   }
 
   if (colType === 'salesInvoice') {
@@ -46,11 +52,11 @@ router.post('/', async (req, res) => {
       endWeek = columnDataName.split('-')[1].split('W')[1]
       year = columnDataName.split('-')[0]
     }
-    response = await getDetail_salesInvoice(program, filters, startWeek, endWeek, year)
+    response = await getDetail_salesInvoice(config, program, filters, startWeek, endWeek, year)
   }
 
   if (colType === 'purchaseOrder') {
-    response = await getDetail_purchaseOrder(program, filters)
+    response = await getDetail_purchaseOrder(config, program, filters)
   }
 
   console.log(`get detail data for ${reportName} route COMPLETE. \n`)
