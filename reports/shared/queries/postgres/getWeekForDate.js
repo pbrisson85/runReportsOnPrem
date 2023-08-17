ec2c370e7a8943f4c7316fb3e80a653816ede60a
Eastern Dev <dev@seaflow.fish>
2023-08-17 07:27:13 -0400

    continued refactoring

--- reports/shared/queries/postgres/getWeekForDate.js
+++ reports/shared/queries/postgres/getWeekForDate.js
@@ -1,24 +1,23 @@
-const sql = require('../../../../server')
-
 const getWeekForDate = async date => {
+  const { Client } = require('pg')
+  const pgClient = new Client() // config from ENV
+  await pgClient.connect()
   console.log(`get week for date: ${date} ...`)
-
-  const response = await sql
+  const response = await pgClient.query(
     `SELECT p.week
      FROM "accountingPeriods".period_by_day AS p
-     WHERE p.formatted_date = ${date}` //prettier-ignore
+     WHERE p.formatted_date = $1`,
+    [date]
+  )
+  await pgClient.end()
 
   // If week is 52 then return 53 since some years have 53 weeks
 
-  console.log(response)
-
-  let week = parseInt(response[0].week)
+  let week = parseInt(response.rows[0].week)
 
   if (week === 52) {
     week = 53
   }
-
   return week
 }
-
 module.exports.getWeekForDate = getWeekForDate
