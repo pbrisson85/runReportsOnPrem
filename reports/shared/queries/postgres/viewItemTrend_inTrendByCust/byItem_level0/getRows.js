8ce40164c7dac25a6949c9a0c106070a2c1e1c28
Eastern Dev <dev@seaflow.fish>
2023-08-18 13:10:31 -0400

    continued refactor

--- reports/shared/queries/postgres/viewItemTrend_inTrendByCust/byItem_level0/getRows.js
+++ reports/shared/queries/postgres/viewItemTrend_inTrendByCust/byItem_level0/getRows.js
@@ -14,7 +14,7 @@ const getRowsFirstLevelDetail = async (config, start, end, program, filters) => 
           
           GROUP BY ms.item_num, ms.description, ms.fg_fresh_frozen, ms.fg_treatment, ms.brand, ms.size_name 
           
-        UNION SELECT ms.item_num AS l1_label, ms.description AS l2_label, ms.fg_fresh_frozen AS l3_label, ms.fg_treatment AS l4_label, ms.brand AS l5_label, ms.size_name AS l6_label, 
+        UNION SELECT ms.item_num AS l1_label, ms.description AS l2_label, ms.fg_fresh_frozen AS l3_label, ms.fg_treatment AS l4_label, ms.brand AS l5_label, ms.size_name AS l6_label 
           FROM "salesReporting".sales_orders 
           LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = sales_orders.item_num 
