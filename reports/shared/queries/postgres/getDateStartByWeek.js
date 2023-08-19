daf9e500750248a85fa1a0f7046c277ad4295095
Eastern Dev <dev@seaflow.fish>
2023-08-19 15:28:44 -0400

    continued refactor

--- reports/shared/queries/postgres/getDateStartByWeek.js
+++ reports/shared/queries/postgres/getDateStartByWeek.js
@@ -1,11 +1,18 @@
-const sql = require('../../../../server')
 const getStartOfWeek = async dateWeekEnd => {
+  const { Client } = require('pg')
+  const pgClient = new Client() // config from ENV
+  await pgClient.connect()
+
   console.log(`query postgres for accounting period start of week ending: ${dateWeekEnd} ...`)
 
-  const periodsByWeek =
-    await sql`SELECT period_by_week.formatted_date_start FROM "accountingPeriods".period_by_week WHERE period_by_week.formatted_date_end = ${dateWeekEnd}`
+  const periodsByWeek = await pgClient.query(
+    'SELECT period_by_week.formatted_date_start FROM "accountingPeriods".period_by_week WHERE period_by_week.formatted_date_end = $1',
+    [dateWeekEnd]
+  )
 
-  return periodsByWeek
+  await pgClient.end()
+
+  return periodsByWeek.rows
 }
 
 module.exports.getStartOfWeek = getStartOfWeek
