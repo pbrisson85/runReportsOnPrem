af66fc6f91f762427dc5dbbfbc7bf23eb170068a
Eastern Dev <dev@seaflow.fish>
2023-08-20 19:12:03 -0400

    continued refactor

--- reports/shared/routines/baseReport.js
+++ reports/shared/routines/baseReport.js
@@ -312,7 +312,7 @@ const buildReport = async (start, end, program, showFyTrend, startWeek, endWeek,
   const rowTemplate_unflat = unflattenByCompositKey(rowTemplate, {
     1: 'l1_label',
     2: 'l2_label',
-    3: 'l3_label',
+    // 3: 'l3_label',
   })
 
   console.log('rowTemplate_unflat', rowTemplate_unflat)
