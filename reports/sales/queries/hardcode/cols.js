dd3e0123dd5453edd480cf19caf2b7ac76ec8598
Eastern Dev <dev@seaflow.fish>
2023-08-24 18:24:22 -0400

    refactoring

--- reports/sales/queries/hardcode/cols.js
+++ reports/sales/queries/hardcode/cols.js
@@ -2,17 +2,17 @@ const getCols = body => {
   const { format } = body
 
   const template = {
-    xjustifyData: 'start', // css justify content
-    xjustifyHeading: 'center', // css justify content
-    xnumber: false, // flag to use formatTableDataNumber model
-    xdecimals: 0, // if number is true, decimals will be used
-    xleftSticky: true, // css sticky
-    xhidden: false, // flag to hide column.
-    xview: null, // dataset to show for the column (extended_cost, weight, cost_per_lb)
-    xdata: false,
-    xcolType: 'label',
-    xoptional: false, // flag to determine if the col is optional
-    xshowByDefault: true, // flag to determine if optional col is shown by default
+    justifyData: 'start', // css justify content
+    justifyHeading: 'center', // css justify content
+    number: false, // flag to use formatTableDataNumber model
+    decimals: 0, // if number is true, decimals will be used
+    leftSticky: true, // css sticky
+    hidden: false, // flag to hide column.
+    view: null, // dataset to show for the column (extended_cost, weight, cost_per_lb)
+    data: false,
+    colType: 'label',
+    optional: false, // flag to determine if the col is optional
+    showByDefault: true, // flag to determine if optional col is shown by default
   }
 
   switch (format) {
