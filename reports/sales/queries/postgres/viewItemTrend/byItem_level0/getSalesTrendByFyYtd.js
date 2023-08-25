6aa9af4f3e6d6d0ce19315b12bd41499720136b6
Eastern Dev <dev@seaflow.fish>
2023-08-25 14:18:31 -0400

    debug

--- reports/sales/queries/postgres/viewItemTrend/byItem_level0/getSalesTrendByFyYtd.js
+++ reports/sales/queries/postgres/viewItemTrend/byItem_level0/getSalesTrendByFyYtd.js
@@ -9,7 +9,7 @@ const lvl_1_subtotal_getSalesByFyYtd = async (config, start, end, program, filte
     console.log(`level 1: (getSalesTrendByFyYtd Lvl0) query postgres to get FG sales data by week ...`)
 
     const response = await sql
-      `SELECT sl.fiscal_year || '_ytd' AS column, ms.item_num AS l1_label, ms.description AS l2_label, ms.fg_fresh_frozen AS l3_label, ms.fg_treatment AS l4_label, ms.brand AS l5_label, ms.size_name AS l6_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
+      `SELECT sl.fiscal_year ${showYtd ? sql`|| '_ytd' ` : sql``} AS column, ms.item_num AS l1_label, ms.description AS l2_label, ms.fg_fresh_frozen AS l3_label, ms.fg_treatment AS l4_label, ms.brand AS l5_label, ms.size_name AS l6_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
       
       FROM "salesReporting".sales_line_items AS sl 
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
@@ -37,7 +37,7 @@ const lvl_0_total_getSalesByFyYtd = async (config, start, end, program, filters,
     console.log(`level 0: (getSalesTrendByFyYtd Lvl0) query postgres to get FG sales data by week ...`)
 
     const response = await sql
-      `SELECT sl.fiscal_year || '_ytd' AS column, 'FG SALES' AS l1_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
+      `SELECT sl.fiscal_year ${showYtd ? sql`|| '_ytd' ` : sql``} AS column, 'FG SALES' AS l1_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
       
       FROM "salesReporting".sales_line_items AS sl 
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
