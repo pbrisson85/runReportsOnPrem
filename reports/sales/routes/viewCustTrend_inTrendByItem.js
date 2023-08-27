31b3d7ae0b71b5fe02a84a6f837a33c0a1f35210
Eastern Dev <dev@seaflow.fish>
2023-08-27 13:51:16 -0400

    refactoring

--- reports/sales/routes/viewCustTrend_inTrendByItem.js
+++ reports/sales/routes/viewCustTrend_inTrendByItem.js
@@ -10,7 +10,7 @@ const { getWeekForDate } = require('../queries/postgres/getWeekForDate')
 router.post('/', async (req, res) => {
   // While in drilldown of an item, can drill down again to return the customeer trend for that item.
 
-  const { program, option, filters, columnDataName, colType, periodEnd, showFyTrend, format } = req.body
+  const { program, option, filters, columnDataName, colType, periodEnd, showFyTrend, format, queryLevel } = req.body
   let { periodStart } = req.body
 
   console.log(`\nget level two drilldown on item for ${format} route HIT...`)
@@ -18,13 +18,10 @@ router.post('/', async (req, res) => {
   // Note that start date is the END of the first week. Need the beginning of the same week to pull invoice dates that are after this:
   const startOfWeek = await getStartOfWeek(periodStart)
   periodStart = startOfWeek[0].formatted_date_start
-
-  let response = null
-
   const startWeek = await getWeekForDate(periodStart) // temporarily until I change the data that is being passed by the front end to the week
   const endWeek = await getWeekForDate(periodEnd) // temporarily until I change the data that is being passed by the front end to the week
 
-  response = await buildDrilldown(filters[0], periodStart, periodEnd, program, filters, showFyTrend, startWeek, endWeek)
+  const response = await buildDrilldown(filters[0], periodStart, periodEnd, program, filters, showFyTrend, startWeek, endWeek, queryLevel)
 
   console.log(`get level two drilldown on item for ${format}  route COMPLETE. \n`)
 
