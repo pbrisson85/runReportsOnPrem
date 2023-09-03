2229a7d871df300347d7759222b3ffd2db603710
Eastern Dev <dev@seaflow.fish>
2023-09-03 09:28:04 -0400

    refactoring trend routes

--- reports/sales/utils/getViewTrendConfig.js
+++ reports/sales/utils/getViewTrendConfig.js
@@ -6,28 +6,28 @@ const getViewTrendConfig = rightMenuSelection => {
   switch (rightMenuSelection) {
     case 'Trend By Item':
       trendQuery = {
-        fileds: ['ms.item_num', 'ms.description', 'ms.fg_fresh_frozen', 'ms.fg_treatment', 'ms.brand', 'ms.size_name'],
+        fields: ['ms.item_num', 'ms.description', 'ms.fg_fresh_frozen', 'ms.fg_treatment', 'ms.brand', 'ms.size_name'],
       }
 
       break
 
     case 'Trend By Customer':
       trendQuery = {
-        fileds: 'ms.species_group',
+        fields: 'ms.species_group',
       }
       d
       break
 
     case 'Trend By Salesperson':
       trendQuery = {
-        fileds: 'ms.species_group',
+        fields: 'ms.species_group',
       }
 
       break
 
     default:
       trendQuery = {
-        fileds: 'ms.species_group',
+        fields: 'ms.species_group',
       }
   }
 
