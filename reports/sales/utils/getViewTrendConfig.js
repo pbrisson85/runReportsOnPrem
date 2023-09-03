0b518fc6c8e686b23fac2908313f1bb27521c99c
Eastern Dev <dev@seaflow.fish>
2023-09-03 08:36:52 -0400

    refactoring trend routes

--- reports/sales/utils/getViewTrendConfig.js
+++ reports/sales/utils/getViewTrendConfig.js
@@ -7,12 +7,18 @@ const getViewTrendConfig = rightMenuSelection => {
     case 'Trend By Item':
       trendQuery = {
         select: [
-          'ms.item_num AS l1_label',
-          'ms.description AS l2_label',
-          'ms.fg_fresh_frozen AS l3_label',
-          'ms.fg_treatment AS l4_label',
-          'ms.brand AS l5_label',
-          'ms.size_name AS l6_label',
+          'ms.item_num',
+          'AS l1_label',
+          'ms.description',
+          'AS l2_label',
+          'ms.fg_fresh_frozen',
+          'AS l3_label',
+          'ms.fg_treatment',
+          'AS l4_label',
+          'ms.brand',
+          'AS l5_label',
+          'ms.size_name',
+          'AS l6_label',
         ],
         groupBy: ['ms.item_num', 'ms.description', 'ms.fg_fresh_frozen', 'ms.fg_treatment', 'ms.brand', 'ms.size_name'],
       }
