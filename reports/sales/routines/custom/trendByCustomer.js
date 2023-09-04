b7e4e66d78daaf42925c7b4d857ee6b9c982e81d
Eastern Dev <dev@seaflow.fish>
2023-09-04 09:07:25 -0400

    adding custom treatment for view trends after doing generic routines

--- reports/sales/routines/custom/trendByCustomer.js
+++ reports/sales/routines/custom/trendByCustomer.js
@@ -1,6 +1,6 @@
 // The problem with pulling the sales by customer is that the customer name can be changed on the sales order. In these cases there will be multiple lines for each customer for each code/name combination. Therefore the getViewTrendConfig for customer only pulls the customer code and not the name. THIS is a routine to add the name at the completion of the query.
 
-const sql = require('../../../../../server')
+const sql = require('../../../../server')
 
 const addCustomerName = async data => {
   // Pull list of customer names from customer master
