0582c2737b8e9f555964ef7dec4dc5813b601197
Eastern Dev <dev@seaflow.fish>
2023-09-06 18:13:17 -0400

    updating generateSales to include ship to county and State. Testing...

--- generateSales/models/cleanStates.js
+++ generateSales/models/cleanStates.js
@@ -1,9 +1,19 @@
 // Filters the order ship to state field for valid states
 
 const cleanStates = (states, orderInfo) => {
-  const validStates = orderInfo.map(order => {
-    return states.includes(order.SHIPTO_STATE)
+  console.log('states', states)
+
+  const statesArr = states.map(state => state.code)
+
+  const validStates = orderInfo.map((order, idx) => {
+    if (idx < 20) {
+      console.log('order', order)
+    }
+
+    return statesArr.includes(order.SHIPTO_STATE)
   })
+
+  console.log('validStates[0]', validStates[0])
 
   return validStates
 }
