7629f99b2a12b90ec094fd5c8b22967a7bb82cdf
Eastern Dev <dev@seaflow.fish>
2023-09-07 11:57:55 -0400

    added route and routine for data checking

--- startup/cron.js
+++ startup/cron.js
@@ -2,7 +2,7 @@ const CronJob = require('cron').CronJob
 const runShipToTests = require('../generateSales/tests/testShipToCountryState')
 
 // define cron jobs
-const runTests = new CronJob('*/2 * * * *', () => runShipToTests(), null, false, 'America/New_York') // update every 10 minutes
+const runTests = new CronJob('*/20 * * * *', () => runShipToTests(), null, false, 'America/New_York') // update every 10 minutes
 
 // add cron.start() here to run the job on app startup
 const runCronOnStartup = () => {
