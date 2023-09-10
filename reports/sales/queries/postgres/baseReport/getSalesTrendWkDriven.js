28b5d43a4836fa3e0a775f15d63f9508b311e33a
Eastern Dev <dev@seaflow.fish>
2023-09-10 11:09:32 -0400

    added year to new trailing weekly ave queries

--- reports/sales/queries/postgres/baseReport/getSalesTrendWkDriven.js
+++ reports/sales/queries/postgres/baseReport/getSalesTrendWkDriven.js
@@ -11,14 +11,14 @@ const l1_getSalesWkDriven = async (config, startWk, endWk, year) => {
     console.log(`${config.user} - level 1: query postgres to get FG sales data period total (l1_getSalesWkDriven) ...`)
 
     const response = await sql
-      `SELECT 'SALES TOTAL' AS column, COALESCE(${sql(config.l1_field)},'BLANK') AS l1_label, 'SUBTOTAL' AS l2_label, 'SUBTOTAL' AS l3_label, 'SUBTOTAL' AS l4_label, COALESCE(SUM(sales_line_items.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sales_line_items.gross_sales_ext),0) AS sales, COALESCE(SUM(sales_line_items.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sales_line_items.othp_ext),0) AS othp 
+      `SELECT 'SALES TOTAL' AS column, COALESCE(${sql(config.l1_field)},'BLANK') AS l1_label, 'SUBTOTAL' AS l2_label, 'SUBTOTAL' AS l3_label, 'SUBTOTAL' AS l4_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
       
-      FROM "salesReporting".sales_line_items 
+      FROM "salesReporting".sales_line_items AS sl
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-          ON ms.item_num = sales_line_items.item_number 
+          ON ms.item_num = sl.item_number 
       
       WHERE 
-        sales_line_items.week >= ${startWk} AND sales_line_items.week <= ${endWk}
+        sl.week >= ${startWk} AND sl.week <= ${endWk}
         AND sl.fiscal_year = ${year} 
         AND ms.byproduct_type IS NULL 
         AND ms.item_type = ${'FG'} 
@@ -43,12 +43,12 @@ const l2_getSalesWkDriven = async (config, startWk, endWk, year) => {
     console.log(`${config.user} - level 2: query postgres to get FG sales data period total (l2_getSalesWkDriven) ...`)
 
     const response = await sql
-      `SELECT 'SALES TOTAL' AS column, COALESCE(${sql(config.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.l2_field)},'NA') AS l2_label, 'SUBTOTAL' AS l3_label, 'SUBTOTAL' AS l4_label, COALESCE(SUM(sales_line_items.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sales_line_items.gross_sales_ext),0) AS sales, COALESCE(SUM(sales_line_items.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sales_line_items.othp_ext),0) AS othp 
+      `SELECT 'SALES TOTAL' AS column, COALESCE(${sql(config.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.l2_field)},'NA') AS l2_label, 'SUBTOTAL' AS l3_label, 'SUBTOTAL' AS l4_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
       
-      FROM "salesReporting".sales_line_items LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sales_line_items.item_number 
+      FROM "salesReporting".sales_line_items AS slLEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sl.item_number 
       
       WHERE 
-        sales_line_items.week >= ${startWk} AND sales_line_items.week <= ${endWk} 
+        sl.week >= ${startWk} AND sl.week <= ${endWk} 
         AND sl.fiscal_year = ${year} 
         AND ms.byproduct_type IS NULL 
         AND ms.item_type = ${'FG'} 
@@ -73,12 +73,12 @@ const l3_getSalesWkDriven = async (config, startWk, endWk, year) => {
     console.log(`${config.user} - level 3: query postgres to get FG sales data period total (l3_getSalesWkDriven) ...`)
 
     const response = await sql
-      `SELECT 'SALES TOTAL' AS column, COALESCE(${sql(config.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.l2_field)},'NA') AS l2_label, COALESCE(${sql(config.l3_field)},'NA') AS l3_label, 'SUBTOTAL' AS l4_label, COALESCE(SUM(sales_line_items.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sales_line_items.gross_sales_ext),0) AS sales, COALESCE(SUM(sales_line_items.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sales_line_items.othp_ext),0) AS othp 
+      `SELECT 'SALES TOTAL' AS column, COALESCE(${sql(config.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.l2_field)},'NA') AS l2_label, COALESCE(${sql(config.l3_field)},'NA') AS l3_label, 'SUBTOTAL' AS l4_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
       
-      FROM "salesReporting".sales_line_items LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sales_line_items.item_number 
+      FROM "salesReporting".sales_line_items AS slLEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sl.item_number 
       
       WHERE 
-        sales_line_items.week >= ${startWk} AND sales_line_items.week <= ${endWk} 
+        sl.week >= ${startWk} AND sl.week <= ${endWk} 
         AND sl.fiscal_year = ${year} 
         AND ms.byproduct_type IS NULL 
         AND ms.item_type = ${'FG'} 
@@ -103,12 +103,12 @@ const l4_getSalesWkDriven = async (config, startWk, endWk, year) => {
     console.log(`${config.user} - level 4: query postgres to get FG sales data period total (l4_getSalesWkDriven) ...`)
 
     const response = await sql
-      `SELECT 'SALES TOTAL' AS column, COALESCE(${sql(config.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.l2_field)},'NA') AS l2_label, COALESCE(${sql(config.l3_field)},'NA') AS l3_label, COALESCE(${sql(config.l4_field)},'NA') AS l4_label, COALESCE(SUM(sales_line_items.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sales_line_items.gross_sales_ext),0) AS sales, COALESCE(SUM(sales_line_items.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sales_line_items.othp_ext),0) AS othp 
+      `SELECT 'SALES TOTAL' AS column, COALESCE(${sql(config.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.l2_field)},'NA') AS l2_label, COALESCE(${sql(config.l3_field)},'NA') AS l3_label, COALESCE(${sql(config.l4_field)},'NA') AS l4_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
       
-      FROM "salesReporting".sales_line_items LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sales_line_items.item_number 
+      FROM "salesReporting".sales_line_items AS slLEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sl.item_number 
       
       WHERE 
-        sales_line_items.week >= ${startWk} AND sales_line_items.week <= ${endWk} 
+        sl.week >= ${startWk} AND sl.week <= ${endWk} 
         AND sl.fiscal_year = ${year} 
         AND ms.byproduct_type IS NULL 
         AND ms.item_type = ${'FG'} 
@@ -133,12 +133,12 @@ const l0_getSalesWkDriven = async (config, startWk, endWk, year) => {
     console.log(`${config.user} - level 0: query postgres to get FG sales data period total (l0_getSalesWkDriven) ...`)
 
     const response = await sql
-      `SELECT 'SALES TOTAL' AS column, 'FG SALES' AS l1_label, 'TOTAL' AS l2_label, 'TOTAL' AS l3_label, 'TOTAL' AS l4_label, COALESCE(SUM(sales_line_items.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sales_line_items.gross_sales_ext),0) AS sales, COALESCE(SUM(sales_line_items.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sales_line_items.othp_ext),0) AS othp 
+      `SELECT 'SALES TOTAL' AS column, 'FG SALES' AS l1_label, 'TOTAL' AS l2_label, 'TOTAL' AS l3_label, 'TOTAL' AS l4_label, COALESCE(SUM(sl.calc_gm_rept_weight),0) AS lbs, COALESCE(SUM(sl.gross_sales_ext),0) AS sales, COALESCE(SUM(sl.cogs_ext_gl),0) AS cogs, COALESCE(SUM(sl.othp_ext),0) AS othp 
       
-      FROM "salesReporting".sales_line_items LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sales_line_items.item_number 
+      FROM "salesReporting".sales_line_items AS slLEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = sl.item_number 
       
       WHERE 
-        sales_line_items.week >= ${startWk} AND sales_line_items.week <= ${endWk} 
+        sl.week >= ${startWk} AND sl.week <= ${endWk} 
         AND sl.fiscal_year = ${year} 
         AND ms.byproduct_type IS NULL 
         AND ms.item_type = ${'FG'} 
