ea8d1af8073138380cb87b8a6e2e46bb4708c2b0
Eastern Dev <dev@seaflow.fish>
2023-09-17 11:00:56 -0400

    testing by product and seconds

--- reports/sales/routines/baseReport.js
+++ reports/sales/routines/baseReport.js
@@ -689,6 +689,8 @@ const buildReport = async (start, end, showFyTrend, startWeek, endWeek, config, 
   Object.keys(mappedData).forEach(key => {
     // If the length = level, then there is only the labels and no other columns are populated on that row
     if (Object.keys(mappedData[key]).length === level) {
+      console.log('deleting: ', mappedData[key])
+
       delete mappedData[key]
     }
   })
