a399b95ae1d08b1f11bd2eae3efa05cbf8aa5c5f
Eastern Dev <dev@seaflow.fish>
2023-09-24 11:46:14 -0400

    added app settings in filters

--- reports/filters/queries/hardcode/appSettings.js
+++ reports/filters/queries/hardcode/appSettings.js
@@ -4,25 +4,39 @@ const appSettings = () => {
       label: 'Shift Totals Labels',
       dataName: 'shiftTotals',
       default: false,
-      onTrueSetToFalse: ['shiftTotalsCss'],
+      onTrueSetToFalse: ['shiftTotalsCss'], // cannot both be true but both can be false
     },
     {
       label: 'Shift Totals Labels and Css',
       dataName: 'shiftTotalsCss',
       default: false,
-      onTrueSetToFalse: ['shiftTotals'],
+      onTrueSetToFalse: ['shiftTotals'], // cannot both be true but both can be false
+    },
+    {
+      label: 'Data Label in Subtotals',
+      dataName: 'dataLabelInSubtotals',
+      default: true,
+      onTrueSetToFalse: false,
+      onFalseSetTrue: ['subtotalLabelInSubtotals'], // cannot both be false but both can be true
+    },
+    {
+      label: 'Subtotal Label in Subtotals',
+      dataName: 'subtotalLabelInSubtotals',
+      default: false,
+      onTrueSetToFalse: false,
+      onFalseSetTrue: ['dataLabelInSubtotals'], // cannot both be false but both can be true
     },
     {
       label: 'Bypass Cache Everyone',
       dataName: 'bypassCacheEveryone',
       default: false,
-      onTrueSetToFalse: ['bypassCacheAdminOnly'],
+      onTrueSetToFalse: ['bypassCacheAdminOnly'], // cannot both be true but both can be false
     },
     {
       label: 'Bypass Cache Admin Only',
       dataName: 'bypassCacheAdminOnly',
       default: true,
-      onTrueSetToFalse: ['bypassCacheEveryone'],
+      onTrueSetToFalse: ['bypassCacheEveryone'], // cannot both be true but both can be false
     },
   ]
 }
