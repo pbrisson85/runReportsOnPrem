b8c19347ab1ded81ea3d4b329f1ad39f0e4feefc
Eastern Dev <dev@seaflow.fish>
2023-09-24 13:22:34 -0400

    added app settings in filters

--- reports/filters/queries/hardcode/appSettings.js
+++ reports/filters/queries/hardcode/appSettings.js
@@ -4,39 +4,43 @@ const appSettings = () => {
       label: 'Shift Totals Labels',
       dataName: 'shiftTotals',
       default: false,
-      onTrueSetToFalse: ['shiftTotalsCss'], // cannot both be true but both can be false
+      onTrueSetFalse: ['shiftTotalsCss'], // cannot both be true but both can be false
+      onFalseSetTrue: false,
     },
     {
       label: 'Shift Totals Labels and Css',
       dataName: 'shiftTotalsCss',
       default: false,
-      onTrueSetToFalse: ['shiftTotals'], // cannot both be true but both can be false
+      onTrueSetFalse: ['shiftTotals'], // cannot both be true but both can be false
+      onFalseSetTrue: false,
     },
     {
       label: 'Data Label in Subtotals',
       dataName: 'dataLabelInSubtotals',
       default: true,
-      onTrueSetToFalse: false,
+      onTrueSetFalse: false,
       onFalseSetTrue: ['subtotalLabelInSubtotals'], // cannot both be false but both can be true
     },
     {
       label: 'Subtotal Label in Subtotals',
       dataName: 'subtotalLabelInSubtotals',
       default: false,
-      onTrueSetToFalse: false,
+      onTrueSetFalse: false,
       onFalseSetTrue: ['dataLabelInSubtotals'], // cannot both be false but both can be true
     },
     {
       label: 'Bypass Cache Everyone',
       dataName: 'bypassCacheEveryone',
       default: false,
-      onTrueSetToFalse: ['bypassCacheAdminOnly'], // cannot both be true but both can be false
+      onTrueSetFalse: ['bypassCacheAdminOnly'], // cannot both be true but both can be false
+      onFalseSetTrue: false,
     },
     {
       label: 'Bypass Cache Admin Only',
       dataName: 'bypassCacheAdminOnly',
       default: true,
-      onTrueSetToFalse: ['bypassCacheEveryone'], // cannot both be true but both can be false
+      onTrueSetFalse: ['bypassCacheEveryone'], // cannot both be true but both can be false
+      onFalseSetTrue: false,
     },
   ]
 }
