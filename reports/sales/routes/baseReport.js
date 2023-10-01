b66cfa524ebded064134048209fc72a90999030d
Eastern Dev <dev@seaflow.fish>
2023-10-01 09:13:03 -0400

    moving cols and all data hardcodes to the back end. this is initial step

--- reports/sales/routes/baseReport.js
+++ reports/sales/routes/baseReport.js
@@ -3,7 +3,7 @@ const buildReport = require('../routines/baseReport')
 const { getStartOfWeek } = require('../queries/postgres/getDateStartByWeek')
 const { getWeekForDate } = require('../queries/postgres/getWeekForDate')
 const getDefaults = require('../utils/getReportDefaults')
-const getCols = require('../queries/hardcode/cols_baseReport')
+const getCols = require('../queries/hardcode/labelCols')
 const getReportConfig = require('../utils/getReportConfig')
 
 // @route   POST /api/sales/byProgram
