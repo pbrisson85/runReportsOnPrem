ffa12ae858d3f67f1c700e2b9dabf29396772e2c
Eastern Dev <dev@seaflow.fish>
2023-10-01 09:19:39 -0400

    moving cols and all data hardcodes to the back end. this is initial step

--- reports/sales/data/detailCols/colsMap.js
+++ reports/sales/data/detailCols/colsMap.js
@@ -4,9 +4,11 @@ const { salesOrderCols } = require('./colsSalesOrder')
 const { salesInvoiceCols } = require('./colsSalesInvoice')
 
 // Note that the key of this map matches the colType of the col
-export const detailColsMap = {
+const detailColsMap = {
   invenFg: invenFgCols,
   purchaseOrder: purchaseOrderCols,
   salesOrder: salesOrderCols,
   salesInvoice: salesInvoiceCols,
 }
+
+module.exports.detailColsMap = detailColsMap
