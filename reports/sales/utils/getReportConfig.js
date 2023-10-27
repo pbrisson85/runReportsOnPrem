e673644fa346ce20f9ea97a1cd064953b4b189dd
Eastern Dev <dev@seaflow.fish>
2023-10-27 07:47:17 -0400

    added seconds for testing

--- reports/sales/utils/getReportConfig.js
+++ reports/sales/utils/getReportConfig.js
@@ -37,7 +37,7 @@ const getReportConfig = reqBody => {
     export: reqBody.export ?? null,
     northAmerica: reqBody.northAmerica ?? null,
     queryLevel: reqBody.queryLevel ?? null,
-    itemType: reqBody.itemType ?? ['FG'],
+    itemType: reqBody.itemType ?? ['FG', 'SECONDS'],
     freshFrozen: reqBody.freshFrozen ?? null,
     custType: reqBody.custType ?? null,
     speciesGroup: reqBody.speciesGroup ?? null,
