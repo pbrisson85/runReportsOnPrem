e6c2ba1cd41395f5546787888ec76fad81f9949b
Eastern Dev <dev@seaflow.fish>
2023-11-03 07:58:24 -0400

    testing with trend options

--- models/calcYoyYtdSalesCol.js
+++ models/calcYoyYtdSalesCol.js
@@ -1,7 +1,11 @@
 const calcYoyYtdSalesCol = (eachRowData, colName) => {
-  console.log('eachRowData', eachRowData)
+  // manually filter out anything that is not 2022/2023
 
-  const yoySales = eachRowData.map((row, idx) => {
+  const filteredData = eachRowData.filter(row => row.column === '2022_ytd' || row.column === '2023_ytd')
+
+  console.log('filteredData', filteredData)
+
+  const yoySales = filteredData.map((row, idx) => {
     //console.log('idx, row', idx, row)
 
     const { lbs, sales, cogs, othp, column, l1_label, l2_label } = row
