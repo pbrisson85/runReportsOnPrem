785c97e10042fbfb273daf6e6bf5ce63e7b01f35
Eastern Dev <dev@seaflow.fish>
2023-11-27 12:45:29 -0500

    commit

--- database/queries/postgres/filters/getDateMaps.js
+++ database/queries/postgres/filters/getDateMaps.js
@@ -115,7 +115,10 @@ const getFiscalYearMap = async () => {
       MAX(p.week) AS wk_last, 
       'fiscal_years' AS map, 
       TRUE AS prevent_filter,
-      CASE WHEN p.fiscal_year = curr_fiscal.fiscal_year THEN TRUE ELSE FALSE END AS default
+      CASE WHEN p.fiscal_year = (
+        SELECT c.fiscal_year
+        FROM "accountingPeriods".period_by_day AS c
+        WHERE c.formatted_date = CURRENT_DATE) THEN TRUE ELSE FALSE END AS default
 
     FROM "accountingPeriods".period_by_day AS p
 
@@ -123,7 +126,7 @@ const getFiscalYearMap = async () => {
       SELECT d.fiscal_year
       FROM "accountingPeriods".period_by_day AS d
       WHERE d.formatted_date = CURRENT_DATE
-      ) AS curr_fiscal
+      )
     GROUP BY fiscal_year
     ORDER BY fiscal_year DESC
       `
