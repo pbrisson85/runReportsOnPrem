67ea3778b40d0838eb65f22c8d6a08ef994b3e3c
Eastern Dev <dev@seaflow.fish>
2023-12-01 08:13:39 -0500

    commit

--- database/queries/postgres/getDateEndPerWeek.js
+++ database/queries/postgres/getDateEndPerWeek.js
@@ -66,4 +66,3 @@ module.exports.getDateEndPerWeekByRange_so = getDateEndPerWeekByRange_so
 module.exports.getDateEndPerWeekByRange_so_tg = getDateEndPerWeekByRange_so_tg
 module.exports.getDateEndPerWeekByRange_so_untg = getDateEndPerWeekByRange_so_untg
 module.exports.getDateEndPerWeekByRange_pj = getDateEndPerWeekByRange_pj
-module.exports.getDateEndPerFiscalPeriodByRange = getDateEndPerFiscalPeriodByRange
