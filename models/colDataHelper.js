42ec3329bdbf8ca2abb384a604fa16a9b8126f51
Eastern Dev <dev@seaflow.fish>
2023-12-03 12:17:50 -0500

    commit

--- models/colDataHelper.js
+++ models/colDataHelper.js
@@ -8,20 +8,16 @@ const addDataToSalesTotalCol = (config, columnConfigs) => {
     const startDisplay = new Date(config.totals.startDatePrimary).toLocaleString('en-US', {
       timeZone: 'America/New_York',
     })
-
     const startDisplayArr = startDisplay.split(',')[0].split('/')
-
-    const startDisplayClean = `${startDisplayArr[0]}/${startDisplayArr[1]}`
+    const startDisplayClean = `${startDisplayArr[0]}/${startDisplayArr[1]}/${startDisplayArr[2]}`
 
     const endDisplay = new Date(config.totals.endDatePrimary).toLocaleString('en-US', {
       timeZone: 'America/New_York',
     })
-
     const endDisplayArr = endDisplay.split(',')[0].split('/')
-
     const endDisplayClean = `${endDisplayArr[0]}/${endDisplayArr[1]}/${endDisplayArr[2]}`
 
-    const displayName = `${col.displayName}-${startDisplayClean}-${endDisplayClean}`
+    const displayName = `${col.displayName} ${startDisplayClean}-${endDisplayClean}`
 
     col.startDate = config.totals.startDatePrimary
     col.endDate = config.totals.endDatePrimary
@@ -47,7 +43,7 @@ const addDataToSoTotalCol = (config, columnConfigs) => {
     const endDisplayArr = endDisplay.split(',')[0].split('/')
     const endDisplayClean = `${endDisplayArr[0]}/${endDisplayArr[1]}/${endDisplayArr[2]}`
 
-    const displayName = `${col.displayName}-${startDisplayClean}-${endDisplayClean}`
+    const displayName = `${col.displayName} ${startDisplayClean}-${endDisplayClean}`
 
     col.startDate = config.salesOrders.startDate
     col.endDate = config.salesOrders.endDate
