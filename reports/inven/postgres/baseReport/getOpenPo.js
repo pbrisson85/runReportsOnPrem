0b652586ef9a114654ef6f36643f30c367288704
Eastern Dev <dev@seaflow.fish>
2023-12-13 18:15:35 -0500

    added inven report

---
+++ reports/inven/postgres/baseReport/getOpenPo.js
@@ -0,0 +1,177 @@
+const sql = require('../../../../server')
+
+/* *********************************************** Level 1 *********************************************** */
+
+const l1_getOpenPo = async config => {
+  //${config.jbBuyer ? sql`AND inv.item_number IN SELECT(DISTINCT(perpetual_inventory.item_number FROM ))` : sql``}
+  try {
+    console.log(`${config.user} - level 1: query postgres for FG open PO (l1_getOpenPo) ...`)
+
+    const response = await sql
+         `SELECT 'PURCHASE ORDER' AS column, COALESCE(${sql(config.baseFormat.l1_field)},'BLANK') AS l1_label, 'SUBTOTAL' AS l2_label, 'SUBTOTAL' AS l3_label, 'SUBTOTAL' AS l4_label, 'SUBTOTAL' AS l5_label, COALESCE(SUM(inv.on_order_lbs),0) AS lbs, COALESCE(SUM(inv.on_order_extended),0) AS cogs 
+        
+         FROM "invenReporting".perpetual_inventory AS inv LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = inv.item_number 
+         
+         WHERE 
+          inv.version = (SELECT MAX(version) - 1 FROM "invenReporting".perpetual_inventory) 
+          AND inv.on_order_lbs <> 0 
+          ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}`: sql``} 
+          ${config.baseFilters.program ? sql`AND ms.program = ${config.baseFilters.program}` : sql``} 
+          ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}
+         
+         GROUP BY ${sql(config.baseFormat.l1_field)}` //prettier-ignore
+
+    return response
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+/* *********************************************** Level 2 *********************************************** */
+
+// FG open PO grouped by program (includes in transit)
+
+const l2_getOpenPo = async config => {
+  try {
+    console.log(`${config.user} - level 2: query postgres for FG open PO (l2_getOpenPo) ...`)
+
+    const response = await sql
+       `SELECT 'PURCHASE ORDER' AS column, COALESCE(${sql(config.baseFormat.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.baseFormat.l2_field)},'NA') AS l2_label, 'SUBTOTAL' AS l3_label, 'SUBTOTAL' AS l4_label, 'SUBTOTAL' AS l5_label, COALESCE(SUM(inv.on_order_lbs),0) AS lbs, COALESCE(SUM(inv.on_order_extended),0) AS cogs 
+       
+       FROM "invenReporting".perpetual_inventory AS inv LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = inv.item_number 
+       
+       WHERE 
+        inv.version = (SELECT MAX(version) - 1 FROM "invenReporting".perpetual_inventory) 
+        AND inv.on_order_lbs <> 0 
+        ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}`: sql``} 
+        ${config.baseFilters.program ? sql`AND ms.program = ${config.baseFilters.program}` : sql``} 
+        ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``} 
+       
+       GROUP BY ${sql(config.baseFormat.l1_field)}, ${sql(config.baseFormat.l2_field)}` //prettier-ignore
+
+    return response
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+/* *********************************************** Level 3 *********************************************** */
+
+// FG open PO grouped by program (includes in transit)
+
+const l3_getOpenPo = async config => {
+  try {
+    console.log(`${config.user} - level 3: query postgres for FG open PO (l3_getOpenPo) ...`)
+
+    const response = await sql
+       `SELECT 'PURCHASE ORDER' AS column, COALESCE(${sql(config.baseFormat.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.baseFormat.l2_field)},'NA') AS l2_label, COALESCE(${sql(config.baseFormat.l3_field)},'NA') AS l3_label, 'SUBTOTAL' AS l4_label, 'SUBTOTAL' AS l5_label, COALESCE(SUM(inv.on_order_lbs),0) AS lbs, COALESCE(SUM(inv.on_order_extended),0) AS cogs 
+       
+       FROM "invenReporting".perpetual_inventory AS inv LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = inv.item_number 
+       
+       WHERE 
+        inv.version = (SELECT MAX(version) - 1 FROM "invenReporting".perpetual_inventory) 
+        AND inv.on_order_lbs <> 0 
+        ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}`: sql``} 
+        ${config.baseFilters.program ? sql`AND ms.program = ${config.baseFilters.program}`: sql``} 
+        ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}
+       
+       GROUP BY ${sql(config.baseFormat.l1_field)}, ${sql(config.baseFormat.l2_field)}, ${sql(config.baseFormat.l3_field)}` //prettier-ignore
+
+    return response
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+/* *********************************************** Level 4 *********************************************** */
+
+// FG open PO grouped by program (includes in transit)
+
+const l4_getOpenPo = async config => {
+  try {
+    console.log(`${config.user} - level 4: query postgres for FG open PO (l4_getOpenPo) ...`)
+
+    const response = await sql
+       `SELECT 'PURCHASE ORDER' AS column, COALESCE(${sql(config.baseFormat.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.baseFormat.l2_field)},'NA') AS l2_label, COALESCE(${sql(config.baseFormat.l3_field)},'NA') AS l3_label, COALESCE(${sql(config.baseFormat.l4_field)},'NA') AS l4_label, 'SUBTOTAL' AS l5_label, COALESCE(SUM(inv.on_order_lbs),0) AS lbs, COALESCE(SUM(inv.on_order_extended),0) AS cogs 
+       
+       FROM "invenReporting".perpetual_inventory AS inv LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = inv.item_number 
+       
+       WHERE 
+        inv.version = (SELECT MAX(version) - 1 FROM "invenReporting".perpetual_inventory) 
+        AND inv.on_order_lbs <> 0 
+        ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}`: sql``} 
+        ${config.baseFilters.program ? sql`AND ms.program = ${config.baseFilters.program}`: sql``} 
+        ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}
+       
+       GROUP BY ${sql(config.baseFormat.l1_field)}, ${sql(config.baseFormat.l2_field)}, ${sql(config.baseFormat.l3_field)}, ${sql(config.baseFormat.l4_field)}` //prettier-ignore
+
+    return response
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+/* *********************************************** Level 4 *********************************************** */
+
+// FG open PO grouped by program (includes in transit)
+
+const l5_getOpenPo = async config => {
+  try {
+    console.log(`${config.user} - level 5: query postgres for FG open PO (l4_getOpenPo) ...`)
+
+    const response = await sql
+       `SELECT 'PURCHASE ORDER' AS column, COALESCE(${sql(config.baseFormat.l1_field)},'BLANK') AS l1_label, COALESCE(${sql(config.baseFormat.l2_field)},'NA') AS l2_label, COALESCE(${sql(config.baseFormat.l3_field)},'NA') AS l3_label, COALESCE(${sql(config.baseFormat.l4_field)},'NA') AS l4_label, COALESCE(${sql(config.baseFormat.l5_field)},'NA') AS l5_label, COALESCE(SUM(inv.on_order_lbs),0) AS lbs, COALESCE(SUM(inv.on_order_extended),0) AS cogs 
+       
+       FROM "invenReporting".perpetual_inventory AS inv LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = inv.item_number 
+       
+       WHERE 
+        inv.version = (SELECT MAX(version) - 1 FROM "invenReporting".perpetual_inventory) 
+        AND inv.on_order_lbs <> 0 
+        ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}`: sql``} 
+        ${config.baseFilters.program ? sql`AND ms.program = ${config.baseFilters.program}`: sql``} 
+        ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}
+       
+       GROUP BY ${sql(config.baseFormat.l1_field)}, ${sql(config.baseFormat.l2_field)}, ${sql(config.baseFormat.l3_field)}, ${sql(config.baseFormat.l4_field)}, ${sql(config.baseFormat.l5_field)}` //prettier-ignore
+
+    return response
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+/* *********************************************** TOTAL *********************************************** */
+
+const l0_getOpenPo = async config => {
+  try {
+    console.log(`${config.user} - level 0: query postgres for FG open PO (l0_getOpenPo) ...`)
+
+    const response = await sql
+         `SELECT 'PURCHASE ORDER' AS column${config.baseFilters.itemType ? sql`, REPLACE('${sql(config.baseFilters.itemType)} SALES','"','') AS l1_label` : sql`,'SALES' AS l1_label`}, 'TOTAL' AS l2_label, 'TOTAL' AS l3_label, 'TOTAL' AS l4_label, 'TOTAL' AS l5_label, COALESCE(SUM(inv.on_order_lbs),0) AS lbs, COALESCE(SUM(inv.on_order_extended),0) AS cogs 
+         
+         FROM "invenReporting".perpetual_inventory AS inv LEFT OUTER JOIN "invenReporting".master_supplement AS ms ON ms.item_num = inv.item_number 
+         
+         WHERE 
+          inv.version = (SELECT MAX(version) - 1 FROM "invenReporting".perpetual_inventory) 
+          AND inv.on_order_lbs <> 0 
+          ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}`: sql``} 
+          ${config.baseFilters.program ? sql`AND ms.program = ${config.baseFilters.program}`: sql``} 
+          ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}` //prettier-ignore
+
+    return response
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+module.exports.l1_getOpenPo = l1_getOpenPo
+module.exports.l2_getOpenPo = l2_getOpenPo
+module.exports.l3_getOpenPo = l3_getOpenPo
+module.exports.l4_getOpenPo = l4_getOpenPo
+module.exports.l5_getOpenPo = l5_getOpenPo
+module.exports.l0_getOpenPo = l0_getOpenPo
