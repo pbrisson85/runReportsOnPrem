325341a75586ed850c74bfebdb7250c67e038380
Eastern Dev <dev@seaflow.fish>
2023-12-15 10:43:41 -0500

    commit

--- reports/utils/configHelpers/getLabelCols.js
+++ reports/utils/configHelpers/getLabelCols.js
@@ -1,4 +1,4 @@
-const getLabelColsTemplate = require('../../data/baseCols/labelColsTemplate')
+const getLabelColsTemplate = require('./labelColsTemplate')
 const getBaseFormatDefault = require('./getBaseFormatDefault')
 
 const getLabelCols = reqBody => {
