cdb1e7dfeb1df1976a82898b9438681f3546ce56
Eastern Dev <dev@seaflow.fish>
2023-12-24 14:44:02 -0500

    refactored-not complete

--- reports/sales/routes/getDetail.js
+++ reports/sales/routes/getDetail.js
@@ -22,10 +22,11 @@ router.post('/', async (req, res) => {
   const { columnDataName, reportFormat, startDate, endDate, useProjection } = req.body
   let { colType } = req.body // for now manually determining if projeciton vs sales below. Need to override col type to projection to get correct cols
 
-  console.log('req.body', req.body)
-
   const config = await getReportConfig(req.body)
+
   console.log('detail report config:', config)
+
+  console.log('req.body', req.body)
 
   let data = null
 
