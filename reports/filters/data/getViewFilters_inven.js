dd222e8f20cd64d471ec234de7c79ed4713e9518
Eastern Dev <dev@seaflow.fish>
2023-12-30 17:59:41 -0500

    commit

--- reports/filters/data/getViewFilters_inven.js
+++ reports/filters/data/getViewFilters_inven.js
@@ -5,36 +5,42 @@ const getViewFilters = () => {
       dataName: 'lbs',
       decimals: 0,
       cols: [], // list of colTypes that are NOT shown in this view
+      default: true,
     },
     {
       label: 'NET REVENUE',
       dataName: 'netSales',
       decimals: 0,
       cols: [],
+      default: false,
     },
     {
       label: 'COST',
       dataName: 'cogs',
       decimals: 0,
       cols: [],
+      default: false,
     },
     {
       label: 'NET REVENUE/LB',
       dataName: 'netSalesPerLb',
       decimals: 2,
       cols: [],
+      default: false,
     },
     {
       label: 'COST/LB',
       dataName: 'cogsPerLb',
       decimals: 2,
       cols: [],
+      default: false,
     },
     {
       label: 'GROSS MARGIN/LB',
       dataName: 'grossMarginPerLb',
       decimals: 2,
       cols: [],
+      default: false,
     },
   ]
 }
