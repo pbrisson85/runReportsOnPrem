f1c815d6f4fb0ee819791650917c69fd77b35339
Eastern Dev <dev@seaflow.fish>
2024-01-02 12:45:56 -0500

    commit

--- reports/sales/postgres/kpi/getWeeksOnHand_slice.js
+++ reports/sales/postgres/kpi/getWeeksOnHand_slice.js
@@ -389,8 +389,6 @@ const l0_getWeeksOnHand = async (config, useProjection) => {
             ${config.baseFilters.queryLevel > 2 ? sql`AND ${sql(config.baseFormat.l3_field)} = ${config.baseFilters.l3_filter}` : sql``}
             ${config.baseFilters.queryLevel > 3 ? sql`AND ${sql(config.baseFormat.l4_field)} = ${config.baseFilters.l4_filter}` : sql``}
             ${config.baseFilters.queryLevel > 4 ? sql`AND ${sql(config.baseFormat.l5_field)} = ${config.baseFilters.l5_filter}` : sql``}
-            
-       
         ) 
             
             
