fc23abdd21dee4d645d8d2bf5148b499569c8c33
Eastern Dev <dev@seaflow.fish>
2024-01-02 17:32:36 -0500

    commit

--- reports/sales/postgres/kpi/getWeeksOnHand_slice.js
+++ reports/sales/postgres/kpi/getWeeksOnHand_slice.js
@@ -159,7 +159,8 @@ const l1_getWeeksOnHand = async (config, trendQuery, useProjection) => {
           ${trendQuery.sl.l6_label ? sql`, pj.l6_label`: sql``} 
           ${trendQuery.sl.l7_label ? sql`, pj.l7_label`: sql``}
         ), 
-            
+        
+        ${trendQuery.inv.l1_label ? sql`
         i AS (
             SELECT 
                 ${trendQuery.inv.l1_label ? sql`${sql(trendQuery.inv.l1_label)} AS l1_label,`: sql``} 
@@ -189,7 +190,7 @@ const l1_getWeeksOnHand = async (config, trendQuery, useProjection) => {
                 ${config.baseFilters.queryLevel > 3 ? sql`AND ${sql(config.baseFormat.l4_field)} = ${config.baseFilters.l4_filter}` : sql``}
                 ${config.baseFilters.queryLevel > 4 ? sql`AND ${sql(config.baseFormat.l5_field)} = ${config.baseFilters.l5_filter}` : sql``}
                 
-            ${trendQuery.inv.l1_label ? sql`GROUP BY`: sql``} 
+            GROUP BY
             ${trendQuery.inv.l1_label ? sql`${sql(trendQuery.inv.l1_label)}`: sql``} 
             ${trendQuery.inv.l2_label ? sql`, ${sql(trendQuery.inv.l2_label)}`: sql``} 
             ${trendQuery.inv.l3_label ? sql`, ${sql(trendQuery.inv.l3_label)}`: sql``} 
@@ -197,7 +198,7 @@ const l1_getWeeksOnHand = async (config, trendQuery, useProjection) => {
             ${trendQuery.inv.l5_label ? sql`, ${sql(trendQuery.inv.l5_label)}`: sql``} 
             ${trendQuery.inv.l6_label ? sql`, ${sql(trendQuery.inv.l6_label)}`: sql``} 
             ${trendQuery.inv.l7_label ? sql`, ${sql(trendQuery.inv.l7_label)}`: sql``}  
-            )
+            )`: sql``}
                 
             SELECT 
             ${dataName} AS column, 
@@ -211,15 +212,16 @@ const l1_getWeeksOnHand = async (config, trendQuery, useProjection) => {
             CASE WHEN s.lbs <= 0 THEN 99999 ELSE COALESCE(i.lbs/NULLIF(s.lbs,0),0) END AS lbs
             
             FROM ave_sales s
-            LEFT OUTER JOIN i
-            ON 
-                ${trendQuery.inv.l1_label ? sql`i.l1_label = s.l1_label`: sql``}
+            ${trendQuery.inv.l1_label ? sql`LEFT OUTER JOIN i
+              ON 
+                i.l1_label = s.l1_label
                 ${trendQuery.inv.l2_label ? sql`AND i.l2_label = s.l2_label`: sql``}
                 ${trendQuery.inv.l3_label ? sql`AND i.l3_label = s.l3_label`: sql``}
                 ${trendQuery.inv.l4_label ? sql`AND i.l4_label = s.l4_label`: sql``}
                 ${trendQuery.inv.l5_label ? sql`AND i.l5_label = s.l5_label`: sql``}
                 ${trendQuery.inv.l6_label ? sql`AND i.l6_label = s.l6_label`: sql``}
                 ${trendQuery.inv.l7_label ? sql`AND i.l7_label = s.l7_label`: sql``}
+            `: sql``}
             ` //prettier-ignore
 
     return response
