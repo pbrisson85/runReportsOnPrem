f221fe551816d9ef1024c4e29817f06799d188a5
Eastern Dev <dev@seaflow.fish>
2024-01-03 07:08:40 -0500

    commit

--- reports/filters/data/getReportFormats.js
+++ reports/filters/data/getReportFormats.js
@@ -924,6 +924,61 @@ const getReportFormats = () => {
         },
       ],
     },
+    {
+      label: 'species / soak / size / brand',
+      dataName: 'specSoakSizeBrnd',
+      defaultsFallback: false,
+      defaults: [],
+      optional: ['any'],
+      forbiddenCols: [],
+      groupingLevel: 3,
+      l1_field: 'ms.species',
+      l2_field: 'ms.fg_treatment',
+      l3_field: 'ms.size_name',
+      l4_field: 'ms.brand',
+      l1_name: 'species',
+      l2_name: 'soak',
+      l3_name: 'size',
+      l3_name: 'brand',
+      labelCols: [
+        {
+          displayName: 'SPECIES', // show as column header
+          dataName: 'l1_label', // key to pull data from
+          filterName: 'l1_filter', // key to match up the column with the filter
+          width: '200px', // css width
+          left: '0px', // css positioning for sticky sum of prior col widths
+          borderRight: false, // border right on ladst frozen cell
+          rightClickMenu: [], // array of options for right click menu
+        },
+        {
+          displayName: 'SOAK', // show as column header
+          dataName: 'l2_label', // key to pull data from
+          filterName: 'l2_filter', // key to match up the column with the filter
+          width: '175px', // css width
+          left: '200px', // css positioning for sticky sum of prior col widths
+          borderRight: false, // border right on ladst frozen cell
+          rightClickMenu: [], // array of options for right click menu
+        },
+        {
+          displayName: 'SIZE', // show as column header
+          dataName: 'l3_label', // key to pull data from
+          filterName: 'l3_filter', // key to match up the column with the filter
+          width: '100px', // css width
+          left: '375px', // css positioning for sticky sum of prior col widths
+          borderRight: false, // border right on ladst frozen cell
+          rightClickMenu: [], // array of options for right click menu
+        },
+        {
+          displayName: 'BRAND', // show as column header
+          dataName: 'l3_label', // key to pull data from
+          filterName: 'l3_filter', // key to match up the column with the filter
+          width: '100px', // css width
+          left: '475px', // css positioning for sticky sum of prior col widths
+          borderRight: true, // border right on ladst frozen cell
+          rightClickMenu: [], // array of options for right click menu
+        },
+      ],
+    },
   ]
 }
 
