639596006908933fecb56c9a80d7a17f2056bcba
Eastern Dev <dev@seaflow.fish>
2024-01-04 12:36:35 -0500

    update

--- reports/inven/routes/baseReport.js
+++ reports/inven/routes/baseReport.js
@@ -2,13 +2,11 @@ const router = require('express').Router()
 const buildReport = require('../routines/baseReport/baseReport')
 const getReportConfig = require('../../utils/getReportConfig')
 
-
 // Generate full weekly report of ALL programs for FG Only (biggest picture)
 router.post('/', async (req, res) => {
   console.log(`\nget get INVEN base report route HIT...`)
 
-  const config = await getReportConfig(req.body)  
-  console.log('inven base report config:', config)
+  const config = await getReportConfig(req.body)
 
   const response = await buildReport(config)
 
