e122badfefa6d475410c6fafc99eb9e9495e3ed1
Eastern Dev <dev@seaflow.fish>
2024-01-04 13:19:23 -0500

    update

--- reports/sales/postgres/getDetail/getInven.js
+++ reports/sales/postgres/getDetail/getInven.js
@@ -8,15 +8,39 @@ const getInven_detail = async config => {
     // Level 2 detail
 
     const response = await sql
-          `SELECT pi.receipt_date, pi.location_date, pi.lot_text, pi.msc_cert_bool AS msc, pi.item_number AS item, pi.description, pi.lot, ms.species, ms.brand, ms.size_name AS size, ms.fg_treatment AS soak, pi.on_hand_lbs AS lbs, pi.cost_lb,  pi.cost_extended AS cost_ext, pi.location_name AS location, pi.location_country as country, ms.fg_fresh_frozen AS fresh_frozen 
+          `SELECT 
+            SUM(ti.weight) AS tagged_lbs,
+            pi.on_hand_lbs - SUM(ti.weight) AS untagged_lbs,
+            pi.receipt_date, 
+            pi.location_date, 
+            pi.lot_text, 
+            pi.msc_cert_bool AS msc, 
+            pi.item_number AS item, 
+            pi.description, 
+            pi.lot, 
+            ms.species, 
+            ms.brand, 
+            ms.size_name AS size, 
+            ms.fg_treatment AS soak, 
+            pi.on_hand_lbs AS lbs, 
+            pi.cost_lb,  
+            pi.cost_extended AS cost_ext, 
+            pi.location_name AS location, 
+            pi.location_country as country, 
+            ms.fg_fresh_frozen AS fresh_frozen 
           
           FROM "invenReporting".perpetual_inventory AS pi 
             LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
               ON ms.item_num = pi.item_number 
+            LEFT OUTER JOIN "salesReporting".tagged_inventory AS ti
+              ON ti.item_num = pi.item_number 
+                AND ti.lot = pi.lot
+                AND ti.location = pi.location_name
               
           WHERE 
             pi.on_hand_lbs <> 0 
             AND pi.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory) 
+            AND ti.version = (SELECT MAX(tagged_inventory.version) - 1 FROM "salesReporting".tagged_inventory) 
             ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}`: sql``} 
             ${config.baseFilters.program ? sql`AND ms.program = ${config.baseFilters.program}`: sql``}
             ${config.trendFilters.speciesGroup ? sql`AND ms.species_group = ${config.trendFilters.speciesGroup}`: sql``}
@@ -30,6 +54,27 @@ const getInven_detail = async config => {
             ${config.baseFilters.queryLevel > 2 ? sql`AND ${sql(config.baseFormat.l3_field)} = ${config.baseFilters.l3_filter}` : sql``}
             ${config.baseFilters.queryLevel > 3 ? sql`AND ${sql(config.baseFormat.l4_field)} = ${config.baseFilters.l4_filter}` : sql``}
             ${config.baseFilters.queryLevel > 4 ? sql`AND ${sql(config.baseFormat.l5_field)} = ${config.baseFilters.l5_filter}` : sql``}
+            
+            GROUP BY
+              pi.receipt_date, 
+              pi.location_date, 
+              pi.lot_text, 
+              pi.msc_cert_bool AS msc, 
+              pi.item_number AS item, 
+              pi.description, 
+              pi.lot, 
+              ms.species, 
+              ms.brand, 
+              ms.size_name AS size, 
+              ms.fg_treatment AS soak, 
+              pi.on_hand_lbs AS lbs, 
+              pi.cost_lb,  
+              pi.cost_extended AS cost_ext, 
+              pi.location_name AS location, 
+              pi.location_country as country, 
+              ms.fg_fresh_frozen AS fresh_frozen 
+            
+            
             ` //prettier-ignore
 
     return response
