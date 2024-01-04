4c00743fa2d21e13a2103131aa56ba69b3b397a0
Eastern Dev <dev@seaflow.fish>
2024-01-04 13:22:44 -0500

    update

--- reports/sales/postgres/getDetail/getInven.js
+++ reports/sales/postgres/getDetail/getInven.js
@@ -59,20 +59,20 @@ const getInven_detail = async config => {
               pi.receipt_date, 
               pi.location_date, 
               pi.lot_text, 
-              pi.msc_cert_bool AS msc, 
-              pi.item_number AS item, 
+              pi.msc_cert_bool, 
+              pi.item_number, 
               pi.description, 
               pi.lot, 
               ms.species, 
               ms.brand, 
-              ms.size_name AS size, 
-              ms.fg_treatment AS soak, 
-              pi.on_hand_lbs AS lbs, 
+              ms.size_name, 
+              ms.fg_treatment, 
+              pi.on_hand_lbs, 
               pi.cost_lb,  
-              pi.cost_extended AS cost_ext, 
-              pi.location_name AS location, 
-              pi.location_country as country, 
-              ms.fg_fresh_frozen AS fresh_frozen 
+              pi.cost_extended, 
+              pi.location_name, 
+              pi.location_country, 
+              ms.fg_fresh_frozen  
             
             
             ` //prettier-ignore
