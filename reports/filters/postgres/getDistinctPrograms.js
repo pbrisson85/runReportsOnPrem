d1a4727461c082b8dba47a6b242103011cf6c407
Eastern Dev <dev@seaflow.fish>
2024-01-07 16:02:14 -0500

    update

--- reports/filters/postgres/getDistinctPrograms.js
+++ reports/filters/postgres/getDistinctPrograms.js
@@ -12,9 +12,9 @@ const getDistinctPrograms = async config => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = sales_line_items.item_number 
       WHERE 
-      1 = 1
-      ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}` : sql``}
-      ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}
+        ms.program IS NOT NULL
+        ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}` : sql``}
+        ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}
 
 
       UNION SELECT 
@@ -24,7 +24,7 @@ const getDistinctPrograms = async config => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = projected_sales.item_number 
       WHERE 
-        1 = 1
+        ms.program IS NOT NULL
         ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}` : sql``}
         ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}
 
@@ -36,7 +36,8 @@ const getDistinctPrograms = async config => {
           LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
             ON ms.item_num = sales_orders.item_num 
       WHERE 
-        sales_orders.version = (SELECT MAX(sales_orders.version) - 1 FROM "salesReporting".sales_orders) 
+        ms.program IS NOT NULL
+        AND sales_orders.version = (SELECT MAX(sales_orders.version) - 1 FROM "salesReporting".sales_orders) 
         ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}` : sql``}  
         ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``}
 
@@ -48,7 +49,8 @@ const getDistinctPrograms = async config => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = perpetual_inventory.item_number 
       WHERE 
-        perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory)
+        ms.program IS NOT NULL
+        AND perpetual_inventory.version = (SELECT MAX(perpetual_inventory.version) - 1 FROM "invenReporting".perpetual_inventory)
         ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}` : sql``}  
         ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``} 
 
@@ -60,7 +62,7 @@ const getDistinctPrograms = async config => {
         LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
           ON ms.item_num = wo.fg_line_item 
       WHERE 
-        1=1
+        ms.program IS NOT NULL
         ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}` : sql``}  
         ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``} 
       ` //prettier-ignore
