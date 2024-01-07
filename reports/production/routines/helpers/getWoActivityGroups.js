8b9e8c9ae6ffc5e25b1468237e619c9370094b0e
Eastern Dev <dev@seaflow.fish>
2024-01-06 19:10:33 -0500

    update

--- reports/production/routines/helpers/getWoActivityGroups.js
+++ reports/production/routines/helpers/getWoActivityGroups.js
@@ -5,14 +5,20 @@ const getWoActivityGroups = async config => {
     console.log(`${config.user} - getWoActivityGroups ...`)
 
     const response = await sql`
-        SELECT 
-          DISTINCT(ms.wo_group) AS wo_group 
-        FROM "invenReporting".master_supplement AS ms 
-          LEFT OUTER JOIN "woReporting".wo_detail_by_fg AS wo
-          ON ms.item_num = wo.fg_line_item
-        WHERE 
-          ms.wo_group IS NOT NULL
-          AND wo.formatted_posting_date >= ${config.totals.primary.startDate} AND wo.formatted_posting_date <= ${config.totals.primary.endDate}
+
+          SELECT 
+            ms.wo_group, ms.item_num
+
+          FROM "woReporting".wo_detail_by_fg AS wo
+            LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
+              ON ms.item_num = wo.wo_activity_code
+          WHERE 
+            ms.wo_group IS NOT NULL 
+            AND wo.formatted_posting_date >= ${config.totals.primary.startDate} AND wo.formatted_posting_date <= ${config.totals.primary.endDate}
+           
+          GROUP BY 
+            ms.wo_group, ms.item_num
+          
       ` //prettier-ignore
 
     return response
