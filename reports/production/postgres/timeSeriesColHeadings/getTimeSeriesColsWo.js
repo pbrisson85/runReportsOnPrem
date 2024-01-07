34d051d2dd5cfd7c8e4befa3f6754ba060386730
Eastern Dev <dev@seaflow.fish>
2024-01-07 08:50:30 -0500

    update

--- reports/production/postgres/timeSeriesColHeadings/getTimeSeriesColsWo.js
+++ reports/production/postgres/timeSeriesColHeadings/getTimeSeriesColsWo.js
@@ -5,30 +5,35 @@ const getTrendColsWo = async (config, woActivityGroups) => {
 
   console.log(`${config.user} - getTrendColsWo`)
 
-  const eachTimeSeries = []
+  try {
+    const eachTimeSeries = []
 
-  for (woActivity of woActivityGroups) {
-    const response = await sql`
-    SELECT 
-      ${sql(config.trends.queryGrouping)} || '_' || ${sql(woActivity)} AS "dataName", 
-        TO_CHAR(MAX(p.formatted_date),'MM/DD/YY') AS "displayName", 
-        MIN(p.formatted_date) AS "colStartDate",  
-        MAX(p.formatted_date) AS "colEndDate",
-        TRUE AS "timeSeriesCol",
-        'wo_' || ${sql(woActivity)} AS "colType" -- should match the colType as the col that drives what is seen on front end via double click
-    
-    FROM "accountingPeriods".period_by_day AS p
-    
-    WHERE p.formatted_date >= ${config.trends.startDate} AND p.formatted_date <= ${config.trends.endDate} 
-    
-    GROUP BY ${sql(config.trends.queryGrouping)}
+    for (woActivity of woActivityGroups) {
+      const response = await sql`
+      SELECT 
+          ${sql(config.trends.queryGrouping)} || '_' || ${sql(woActivity)} AS "dataName", 
+          TO_CHAR(MAX(p.formatted_date),'MM/DD/YY') AS "displayName", 
+          MIN(p.formatted_date) AS "colStartDate",  
+          MAX(p.formatted_date) AS "colEndDate",
+          TRUE AS "timeSeriesCol",
+          'wo_' || ${sql(woActivity)} AS "colType" -- should match the colType as the col that drives what is seen on front end via double click
+      
+      FROM "accountingPeriods".period_by_day AS p
+      
+      WHERE p.formatted_date >= ${config.trends.startDate} AND p.formatted_date <= ${config.trends.endDate} 
+      
+      GROUP BY ${sql(config.trends.queryGrouping)}
+  
+      ORDER BY ${sql(config.trends.queryGrouping)} ASC`
 
-    ORDER BY ${sql(config.trends.queryGrouping)} ASC`
+      eachTimeSeries.push(...response)
+    }
 
-    eachTimeSeries.push(...response)
+    return eachTimeSeries
+  } catch (error) {
+    console.log(error)
+    console.log(error.query)
   }
-
-  return eachTimeSeries
 }
 
 module.exports = { getTrendColsWo }
