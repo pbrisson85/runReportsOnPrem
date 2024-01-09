b4f869b5adb7399a1f078a9cd1a794f15ce98357
Eastern Dev <dev@seaflow.fish>
2024-01-09 18:35:50 -0500

    added menu filter for production country

--- reports/filters/postgres/getProductionCountryFilters.js
+++ reports/filters/postgres/getProductionCountryFilters.js
@@ -8,15 +8,11 @@ const getProductionCountryFilters = async config => {
         DISTINCT(TRIM(ms.program_country)) AS label, 
         (TRIM(ms.program_country)) AS "dataName",
         TRUE AS default 
-    FROM "salesReporting".sales_line_items 
-        LEFT OUTER JOIN "invenReporting".master_supplement AS ms 
-            ON ms.item_num = sales_line_items.item_number 
+    FROM "invenReporting".master_supplement AS ms 
     WHERE 
-    ms.program_country IS NOT NULL 
-    AND ms.wo_entity IS NOT NULL
-    ${config.baseFilters.itemType ? sql`AND ms.item_type IN ${sql(config.baseFilters.itemType)}` : sql``} 
-    ${config.userPermissions.joeB ? sql`AND ms.item_num IN (SELECT jb.item_number FROM "purchaseReporting".jb_purchase_items AS jb)` : sql``} 
-    
+      ms.program_country IS NOT NULL 
+      AND ms.wo_entity IS NOT NULL
+      
     `
 
   return filters
