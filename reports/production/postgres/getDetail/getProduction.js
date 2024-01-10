dbd5dd9e63f538f2566cf159fd97c9c151abc9fe
Eastern Dev <dev@seaflow.fish>
2024-01-10 07:00:12 -0500

    added menu filter for production country

--- reports/production/postgres/getDetail/getProduction.js
+++ reports/production/postgres/getDetail/getProduction.js
@@ -76,7 +76,8 @@ const getProduction_detail = async (config, startDate, endDate, woActivity) => {
         ${config.baseFilters.queryLevel > 1 ? sql`AND ${sql(config.baseFormat.l2_field)} = ${config.baseFilters.l2_filter}` : sql``} 
         ${config.baseFilters.queryLevel > 2 ? sql`AND ${sql(config.baseFormat.l3_field)} = ${config.baseFilters.l3_filter}` : sql``}
         ${config.baseFilters.queryLevel > 3 ? sql`AND ${sql(config.baseFormat.l4_field)} = ${config.baseFilters.l4_filter}` : sql``}
-        ${config.baseFilters.queryLevel > 4 ? sql`AND ${sql(config.baseFormat.l5_field)} = ${config.baseFilters.l5_filter}` : sql``} 
+        ${config.baseFilters.queryLevel > 4 ? sql`AND ${sql(config.baseFormat.l5_field)} = ${config.baseFilters.l5_filter}` : sql``}
+        ${config.baseFilters.productionCountries ? sql`AND act.program_country IN ${sql(config.baseFilters.productionCountries)}`: sql``} 
       ` //prettier-ignore
 
     return response
