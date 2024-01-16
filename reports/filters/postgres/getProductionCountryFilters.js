ae6a74fadf6f21038385960dc640f3806606c21e
Eastern Dev <dev@seaflow.fish>
2024-01-16 15:30:53 -0500

    adding work order activity filter

--- reports/filters/postgres/getProductionCountryFilters.js
+++ reports/filters/postgres/getProductionCountryFilters.js
@@ -4,16 +4,28 @@ const getProductionCountryFilters = async config => {
   console.log(`query postgres for getProductionCountryFilters ...`)
 
   const filters = await sql` 
+    WITH REVERSED_ROWS AS (
+      SELECT 
+        DISTINCT(TRIM(ms.program_country)) AS label,
+        ROW_NUMBER() OVER (ORDER BY ms.program_country DESC) AS reversed_row_number
+      FROM "invenReporting".master_supplement AS ms
+      WHERE 
+        ms.program_country IS NOT NULL 
+        AND ms.wo_entity IS NOT NULL
+    )
+
     SELECT 
-        DISTINCT(TRIM(ms.program_country)) AS label, 
-        (TRIM(ms.program_country)) AS "dataName",
-        TRUE AS default,
-        TRUE AS "trueOnNoSelection" 
-    FROM "invenReporting".master_supplement AS ms 
-    WHERE 
-      ms.program_country IS NOT NULL 
-      AND ms.wo_entity IS NOT NULL
-      
+        label,
+        label AS "dataName", 
+        CASE
+          WHEN reversed_row_number = 1 THEN true
+          ELSE false
+        END AS default,
+        CASE
+          WHEN reversed_row_number = 1 THEN true
+          ELSE false
+        END AS "trueOnNoSelection"
+    FROM REVERSED_ROWS 
     `
 
   return filters
