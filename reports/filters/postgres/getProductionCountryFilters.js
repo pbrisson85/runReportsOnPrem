f558e8fc9c2c8f6772307dca059e6a5e56b9a355
Eastern Dev <dev@seaflow.fish>
2024-01-16 15:34:53 -0500

    adding work order activity filter

--- reports/filters/postgres/getProductionCountryFilters.js
+++ reports/filters/postgres/getProductionCountryFilters.js
@@ -6,12 +6,14 @@ const getProductionCountryFilters = async config => {
   const filters = await sql` 
     WITH REVERSED_ROWS AS (
       SELECT 
-        DISTINCT(TRIM(ms.program_country)) AS label,
+        TRIM(ms.program_country) AS label,
         ROW_NUMBER() OVER (ORDER BY ms.program_country DESC) AS reversed_row_number
       FROM "invenReporting".master_supplement AS ms
       WHERE 
         ms.program_country IS NOT NULL 
         AND ms.wo_entity IS NOT NULL
+      GROUP BY
+        ms.program_country
     )
 
     SELECT 
