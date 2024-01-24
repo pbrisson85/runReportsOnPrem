144ea0195295103ec483288b3ceb5e90e9cf46e6
Eastern Dev <dev@seaflow.fish>
2024-01-24 08:43:17 -0500

    working on production slice

--- reports/production/routines/rowSlice/import.js
+++ reports/production/routines/rowSlice/import.js
@@ -3,11 +3,11 @@ module.exports = {
   getColumns: require('../helpers/getColumns'),
 
   // Production Data
-  l1_getProduction: require('../../postgres/baseReport/getProductionFg').l1_getProduction,
-  l0_getProduction: require('../../postgres/baseReport/getProductionFg').l0_getProduction,
+  l1_getProduction: require('../../postgres/rowSlice/getProductionFg').l1_getProduction,
+  l0_getProduction: require('../../postgres/rowSlice/getProductionFg').l0_getProduction,
 
-  l1_getProductionTrend: require('../../postgres/baseReport/getProductionTrendFg').l1_getProductionTrend,
-  l0_getProductionTrend: require('../../postgres/baseReport/getProductionTrendFg').l0_getProductionTrend,
+  l1_getProductionTrend: require('../../postgres/rowSlice/getProductionTrendFg').l1_getProductionTrend,
+  l0_getProductionTrend: require('../../postgres/rowSlice/getProductionTrendFg').l0_getProductionTrend,
 
   // Inv Data
   l1_getInv: require('../../postgres/rowSlice/getInven').l1_getInv,
@@ -17,15 +17,15 @@ module.exports = {
   l1_getOpenPo: require('../../postgres/rowSlice/getOpenPo').l1_getOpenPo,
   l0_getOpenPo: require('../../postgres/rowSlice/getOpenPo').l0_getOpenPo,
 
-  l1_getReceivedPo: require('../../postgres/baseReport/getReceivedPo').l1_getReceivedPo,
-  l0_getReceivedPo: require('../../postgres/baseReport/getReceivedPo').l0_getReceivedPo,
+  l1_getReceivedPo: require('../../postgres/rowSlice/getReceivedPo').l1_getReceivedPo,
+  l0_getReceivedPo: require('../../postgres/rowSlice/getReceivedPo').l0_getReceivedPo,
 
   // Sales Order Total Data
   l1_getSo: require('../../postgres/rowSlice/getSo').l1_getSo,
   l0_getSo: require('../../postgres/rowSlice/getSo').l0_getSo,
 
-  l1_getSalesTotalPrimary: require('../../postgres/baseReport/getSales').l1_getSalesTotalPrimary,
-  l0_getSalesTotalPrimary: require('../../postgres/baseReport/getSales').l0_getSalesTotalPrimary,
+  l1_getSalesTotalPrimary: require('../../postgres/rowSlice/getSales').l1_getSalesTotalPrimary,
+  l0_getSalesTotalPrimary: require('../../postgres/rowSlice/getSales').l0_getSalesTotalPrimary,
 
   // KPI
   l0_getWeeksOnHand: require('../../postgres/kpi/getWeeksOnHand_slice').l0_getWeeksOnHand,
