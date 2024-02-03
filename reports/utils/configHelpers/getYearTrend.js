35166901b419282ca02f792dc4d348e97e67d500
Eastern Dev <dev@seaflow.fish>
2024-02-03 10:33:28 -0500

    commit

--- reports/utils/configHelpers/getYearTrend.js
+++ reports/utils/configHelpers/getYearTrend.js
@@ -1,7 +1,7 @@
 const getYearTrend = reqBody => {
-  if (typeof reqBody.trendOption?.[0]?.ytd === 'undefined') return null
+  console.log('trendStartPrimary', reqBody.trendStartPrimary)
 
-  console.log('reqBody', reqBody)
+  if (typeof reqBody.trendOption?.[0]?.ytd === 'undefined') return null
 
   return {
     period_name: reqBody.trendOption[0].ytdPeriodField, // will have a start period and end period. period field name will be determined based on cal vs fiscal (month or week)
