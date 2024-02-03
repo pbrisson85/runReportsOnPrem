d0b2bc44cca8a276e3e545efa3e6fc201486f06e
Eastern Dev <dev@seaflow.fish>
2024-02-03 12:37:49 -0500

    commit

--- reports/utils/configHelpers/getYearTrend.js
+++ reports/utils/configHelpers/getYearTrend.js
@@ -2,11 +2,11 @@ const getYearTrend = reqBody => {
   if (typeof reqBody.trendOption?.[0]?.yoy === 'undefined') return null
 
   console.log('reqBody', reqBody)
-  console.log('reqBody.trendStartPrimary', reqBody.trendStartPrimary)
+  console.log('reqBody.yoyStartPrimary', reqBody.trendStartPrimary)
 
   return {
     period_name: reqBody.trendOption[0].yoyPeriodField, // will have a start period and end period. period field name will be determined based on cal vs fiscal (month or week)
-    start_period: parseInt(reqBody.trendStartPrimary?.period),
+    start_period: parseInt(reqBody?.yoyStartPrimary?.period) ?? 1,
     end_period: parseInt(reqBody.trendEndPrimary?.period) === 52 ? 53 : parseInt(reqBody.trendEndPrimary?.period),
     years: typeof reqBody.trendYears === 'undefined' ? [] : reqBody.trendYears.map(yr => parseInt(yr)),
   }
