f7ab32879cdc31884d832a037deb629c66ae24a5
Eastern Dev <dev@seaflow.fish>
2024-02-05 07:53:54 -0500

    commit

--- reports/filters/data/getItemTypeDefaults_production.js
+++ reports/filters/data/getItemTypeDefaults_production.js
@@ -1,7 +1,7 @@
 // item types are pulled from db but defaults are manually added here.
 
 const getItemTypeDefaults = () => {
-  const defaults = ['FG', 'SECONDS', 'WO_ACTIVITY', 'WIP', 'RM']
+  const defaults = ['FG', 'SECONDS', 'WIP']
 
   return defaults
 }
