1bbf1c14f78e346c2fccb5062f6771df95748912
Eastern Dev <dev@seaflow.fish>
2024-03-29 10:16:42 -0400

    commit - working on othp by gl

--- reports/sales/postgres/baseReport/helperQueries/getDataForTempTable.js
+++ reports/sales/postgres/baseReport/helperQueries/getDataForTempTable.js
@@ -4,23 +4,13 @@ const getDataForTempTable = async (config, uniqueOthpGlsArray) => {
   try {
     console.log(`${config.user} - getDataForTempTable ...`)
 
-    let selectString = ''
-
-    for (gl of uniqueOthpGlsArray) {
-      if (selectString === '') {
-        selectString = `SUM(CASE WHEN scl.othp_gl = '${gl.othp_gl}' THEN scl.othp_amount ELSE 0 END) AS ${gl.display_name}`
-      } else {
-        selectString = `${selectString}, SUM(CASE WHEN scl.othp_gl = '${gl.othp_gl}' THEN scl.othp_amount ELSE 0 END) AS ${gl.display_name}`
-      }
-    }
-
     const response = await sql
       `
       SELECT
           scl.invoice_num,
           scl.invoice_line,
-          scl.othp_amount,
-          ${sql`${selectString}`}
+          scl.othp_amount
+          ${sql`${uniqueOthpGlsArray.map(gl => sql`, SUM(CASE WHEN scl.othp_gl = '${gl.othp_gl}' THEN scl.othp_amount ELSE 0 END) AS ${gl.display_name}`)}`}
       FROM "salesReporting".sales_contra_lines AS scl
           LEFT OUTER JOIN "salesReporting".sales_line_items AS sl
           ON scl.invoice_num = sl.invoice_number AND scl.invoice_line = sl.line_number
