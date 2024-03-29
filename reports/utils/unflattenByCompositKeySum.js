43ebeb65f3f39526fb803e35981c8d754f243226
Eastern Dev <dev@seaflow.fish>
2024-03-29 18:05:57 -0400

    commit - working on othp by gl

--- reports/utils/unflattenByCompositKeySum.js
+++ reports/utils/unflattenByCompositKeySum.js
@@ -4,7 +4,6 @@ const unflattenByCompositKeySum = (data, config) => {
   // create list of keys from config passed in
   const vals = Object.values(config)
 
-  let dupKeyCounter = 0
   data.forEach(row => {
     // build key
     let key = null
@@ -23,18 +22,10 @@ const unflattenByCompositKeySum = (data, config) => {
       const keys = Object.keys(row)
 
       keys.forEach(k => {
-        if (dupKeyCounter <= 20) console.log('dupKeyCounter', dupKeyCounter) //DEBUG
-        if (dupKeyCounter <= 20) console.log('k', k) //DEBUG
-        if (dupKeyCounter <= 20) console.log('typeof unflat[key][k]', typeof unflat[key][k]) //DEBUG
-
         if (typeof unflat[key][k] === 'number') {
-          if (dupKeyCounter <= 20) console.log('unflat[key][k]', unflat[key][k]) //DEBUG
-          if (dupKeyCounter <= 20) console.log('row[k]', row[k]) //DEBUG
-
-          unflat[key][k] += row[k]
+          unflat[key][k] = unflat[key][k] + row[k]
         }
       })
-      dupKeyCounter++
     }
   })
 
