3131b522c7eb77b33b8fa0674a10e4b6ef6f516d
Eastern Dev <dev@seaflow.fish>
2024-03-29 08:57:16 -0400

    commit - working on othp by gl

--- reports/sales/postgres/baseReport/helperQueries/createTempTable.js
+++ reports/sales/postgres/baseReport/helperQueries/createTempTable.js
@@ -8,33 +8,28 @@ const createTempTable = async (config, uniqueOthpGlsArray) => {
     const tmpTableName = '"salesReporting".sales_contra_lines_temp_' + tag
     const constraint = `${tmpTableName}_pkey`
 
-    let createString = `CREATE TABLE IF NOT EXISTS ${tmpTableName} `
-    let colString = `(
+    // let createString = `CREATE TABLE IF NOT EXISTS ${tmpTableName} `
+    // let colString = `(
+    //   invoice_num character varying(255) NOT NULL,
+    //   invoice_line character varying(255) NOT NULL,
+    //   othp_amount numeric NOT NULL,`
+
+    // for (gl of uniqueOthpGlsArray) {
+    //   colString = `${colString} ${gl.display_name} numeric NOT NULL,`
+    // }
+
+    // let constraintString = `CONSTRAINT ${constraint} PRIMARY KEY (invoice_num, invoice_line))`
+
+    // let queryString = `${createString} ${colString} ${constraintString}`
+
+    const response = await sql`CREATE TABLE IF NOT EXISTS ${sql(tmpTableName)}
+    (
       invoice_num character varying(255) NOT NULL,
       invoice_line character varying(255) NOT NULL,
-      othp_amount numeric NOT NULL,`
-
-    for (gl of uniqueOthpGlsArray) {
-      colString = `${colString} ${gl.display_name} numeric NOT NULL,`
-    }
-
-    let constraintString = `CONSTRAINT ${constraint} PRIMARY KEY (invoice_num, invoice_line))`
-
-    let queryString = `${createString} ${colString} ${constraintString}`
-
-    // const response = await sql
-    // `
-    //   CREATE TABLE IF NOT EXISTS ${sql(tmpTableName)}
-    //   (
-    //       invoice_num character varying(255) NOT NULL,
-    //       invoice_line character varying(255) NOT NULL,
-    //       othp_amount numeric NOT NULL,
-    //       ${sql()}
-    //       CONSTRAINT ${sql(constraint)} PRIMARY KEY (invoice_num, invoice_line)
-    //   )
-    // ` //prettier-ignore
-
-    const response = await sql`${sql(queryString)}`
+      othp_amount numeric NOT NULL,
+      ${sql`${uniqueOthpGlsArray.map(gl => sql`${gl.display_name} numeric NOT NULL,`)}`}
+      CONSTRAINT ${sql`${constraint}`} PRIMARY KEY (invoice_num, invoice_line)
+    )` //prettier-ignore
 
     return tmpTableName
   } catch (error) {
