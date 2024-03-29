c2d8044130a290756aaa4a48c117e7b915377a43
Eastern Dev <dev@seaflow.fish>
2024-03-29 18:16:45 -0400

    commit - working on othp by gl

--- reports/sales/postgres/baseReport/helperQueries/getDataForTempTable.js
+++ reports/sales/postgres/baseReport/helperQueries/getDataForTempTable.js
@@ -9,7 +9,7 @@ const getDataForTempTable = async (config, uniqueOthpGlsArray) => {
       SELECT
           scl.invoice_num,
           scl.invoice_line,
-          scl.othp_amount
+          SUM(scl.othp_amount) AS othp_amount,
           ${sql`${uniqueOthpGlsArray.map(gl => sql`, SUM(CASE WHEN scl.othp_gl = ${gl.othp_gl} THEN scl.othp_amount ELSE 0 END) AS ${sql(gl.display_name)}`)}`}
       FROM "salesReporting".sales_contra_lines AS scl
           LEFT OUTER JOIN "salesReporting".sales_line_items AS sl
@@ -17,7 +17,7 @@ const getDataForTempTable = async (config, uniqueOthpGlsArray) => {
       WHERE 
           sl.formatted_invoice_date >= ${config.dates.totals.primary.startDate} 
           AND sl.formatted_invoice_date <= ${config.dates.totals.primary.endDate}
-      GROUP BY scl.invoice_num, scl.invoice_line, scl.othp_amount
+      GROUP BY scl.invoice_num, scl.invoice_line
         ` //prettier-ignore
 
     return response
