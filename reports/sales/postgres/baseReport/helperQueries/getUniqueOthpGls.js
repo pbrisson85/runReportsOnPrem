8a1e10f132c870e134dde74e088964716d407901
Eastern Dev <dev@seaflow.fish>
2024-04-01 08:53:03 -0400

    commit - working on othp by gl

--- reports/sales/postgres/baseReport/helperQueries/getUniqueOthpGls.js
+++ reports/sales/postgres/baseReport/helperQueries/getUniqueOthpGls.js
@@ -7,11 +7,9 @@ const getUniqueOthpGls = async config => {
     const response = await sql
       `SELECT ct.othp_gl
         FROM "salesReporting".sales_contra_lines AS ct
-            LEFT OUTER JOIN "salesReporting".sales_line_items AS sl
-            ON ct.invoice_num = sl.invoice_number AND ct.invoice_line = sl.line_number
         WHERE 
-            sl.formatted_invoice_date >= ${config.dates.totals.primary.startDate} 
-            AND sl.formatted_invoice_date <= ${config.dates.totals.primary.endDate} 
+            ct.formatted_invoice_date >= ${config.dates.totals.primary.startDate} 
+            AND ct.formatted_invoice_date <= ${config.dates.totals.primary.endDate} 
         GROUP BY ct.othp_gl 
         ` //prettier-ignore
 
