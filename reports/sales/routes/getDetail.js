65659ffb8790fdb2475d824f40273b5dc73ce251
Eastern Dev <dev@seaflow.fish>
2024-04-09 13:01:18 -0400

    experimenting with adding othp detail categories to sales get detail

--- reports/sales/routes/getDetail.js
+++ reports/sales/routes/getDetail.js
@@ -106,15 +106,9 @@ router.post('/', async (req, res) => {
   const detailColsMap = await getDetailColsMap() // This is using a function which is different from all other modules because I am dynamically building the sales cols due to othp
 
   let cols = detailColsMap[colType]
-
   // remove cols with no data (because all othp cals are being added to template but I dont like how it shows as NAN)
   const dataCols = Object.keys(data[0])
-  const colKeys = Object.keys(cols)
-  const colsToRemove = colKeys.filter(col => !dataCols.includes(col))
-
-  console.log('colsToRemove', colsToRemove)
-
-  cols = cols.filter(col => !colsToRemove.includes(col.dataName))
+  cols = cols.filter(col => !dataCols.includes(col.dataName))
 
   const menu = groupByOptions[colType]
 
